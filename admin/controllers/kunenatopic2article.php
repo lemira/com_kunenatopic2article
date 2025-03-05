f413392274a26e1129a91f2ab2683052e737010e
lemira <lemirat@gmail.com>
2025-03-05 21:30:51 +0100

    Create kunenatopic2article.php
    Контроллер для административной части

---
+++ admin/controllers/kunenatopic2article.php
@@ -0,0 +1,14 @@
+<?php
+defined('_JEXEC') or die;
+
+use Joomla\CMS\MVC\Controller\AdminController;
+
+class KunenaTopic2ArticleController extends AdminController
+{
+    protected $default_view = 'topics';
+
+    public function getModel($name = 'Topic', $prefix = 'KunenaTopic2ArticleModel', $config = array('ignore_request' => true))
+    {
+        return parent::getModel($name, $prefix, $config);
+    }
+}
