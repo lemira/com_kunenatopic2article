d66ec23cb6ac54ef2ac5aab00d47002a62bb009e
lemira <lemirat@gmail.com>
2025-03-06 21:12:35 +0100

    Update kunenatopic2article.php
    грок

--- admin/kunenatopic2article.php
+++ admin/kunenatopic2article.php
@@ -1,14 +1,8 @@
 <?php
 defined('_JEXEC') or die;
-
 use Joomla\CMS\Factory;
-use Joomla\CMS\MVC\Controller\BaseController;
-
-if (!Factory::getUser()->authorise('core.manage', 'com_kunenatopic2article'))
-{
-    throw new Exception(JText::_('JERROR_ALERTNOAUTHOR'), 403);
-}
-
-$controller = BaseController::getInstance('KunenaTopic2Article');
+// Загружаем контроллер явно
+JLoader::register('KunenaTopic2ArticleController', JPATH_COMPONENT . '/controllers/KunenaTopic2Article.php');
+$controller = JControllerLegacy::getInstance('KunenaTopic2Article');
 $controller->execute(Factory::getApplication()->input->get('task'));
 $controller->redirect();
