3f2bfd4c6dc9c9490d48c4a9b6ceaa4b8e7d032a
Leonid <lemirat@gmail.com>
2025-03-07 15:53:05 +0100

    Добавлен шаблон default.php

---
+++ admin/views/topics/tmpl/default.php
@@ -0,0 +1,7 @@
+<?php
+defined('_JEXEC') or die;
+?>
+<div class="container-fluid">
+    <h1>Kunena Topic to Article</h1>
+    <p>Компонент работает! Скоро здесь будет форма для создания статей.</p>
+</div>
