e962b52390c6108c19568644aa760bc558cca0f5
lemira <lemirat@gmail.com>
2025-03-08 14:42:22 +0100

    Update KunenaTopic2Article.php
    Updated controller to display topics view

--- admin/controllers/KunenaTopic2Article.php
+++ admin/controllers/KunenaTopic2Article.php
@@ -7,7 +7,9 @@ class KunenaTopic2ArticleController extends BaseController
 {
     public function display($cachable = false, $urlparams = [])
     {
-        echo "Controller loaded successfully!";
-        jexit();
+        // Устанавливаем вьюху (topics) для отображения
+        $view = $this->input->get('view', 'topics');
+        $this->input->set('view', $view);
+        return parent::display($cachable, $urlparams);
     }
 }
