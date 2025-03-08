e2d615f3b4c02e22a104f69e01ef0e73ba2198c2
lemira <lemirat@gmail.com>
2025-03-08 11:33:43 +0100

    Update Topic.php
    Updated Topic model to use ListModel for params table

--- admin/models/Topic.php
+++ admin/models/Topic.php
@@ -1,19 +1,26 @@
 <?php
 defined('_JEXEC') or die;
 
-use Joomla\CMS\MVC\Model\AdminModel;
+use Joomla\CMS\MVC\Model\ListModel;
 
-class KunenaTopic2ArticleModelTopic extends AdminModel
+class KunenaTopic2ArticleModelTopic extends ListModel
 {
     protected $text_prefix = 'COM_KUNENATOPIC2ARTICLE';
 
-    public function getTable($type = 'Topic', $prefix = 'KunenaTopic2ArticleTable', $config = array())
+    public function __construct($config = [])
     {
-        return JTable::getInstance($type, $prefix, $config);
+        if (empty($config['filter_fields'])) {
+            $config['filter_fields'] = ['id', 'topic_selection', 'article_category'];
+        }
+        parent::__construct($config);
     }
 
-    protected function prepareTable($table)
+    protected function getListQuery()
     {
-        $table->title = htmlspecialchars_decode($table->title, ENT_QUOTES);
+        $db = $this->getDbo();
+        $query = $db->getQuery(true);
+        $query->select('*')
+              ->from($db->quoteName('#__kunenatopic2article_params'));
+        return $query;
     }
 }
