f4eec2e624e0cd356b1acba9f1a2ae72abcdde53
lemira <lemirat@gmail.com>
2025-03-09 15:08:26 +0100

    Update default.php
    Обновление формы

--- admin/views/topics/tmpl/default.php
+++ admin/views/topics/tmpl/default.php
@@ -1,7 +1,42 @@
 <?php
-defined('_JEXEC') or die;
+defined('_JEXEC') or die('Restricted access');
+
+JHtml::_('behavior.formvalidation');
+JHtml::_('formbehavior.chosen', 'select');
 ?>
-<div class="container-fluid">
-    <h1>Kunena Topic to Article</h1>
-    <p>Компонент работает! Скоро здесь будет форма для создания статей.</p>
-</div>
+<form action="index.php?option=com_kunenatopic2article&task=kunenatopic2article.save" method="post" id="adminForm" class="form-validate">
+    <div class="row-fluid">
+        <div class="span12">
+            <fieldset class="adminform">
+                <legend><?php echo JText::_('COM_KUNENATOPIC2ARTICLE_ARTICLE_FORM'); ?></legend>
+                <div class="control-group">
+                    <div class="control-label">
+                        <?php echo $this->form->getLabel('topic_id'); ?>
+                    </div>
+                    <div class="controls">
+                        <?php echo $this->form->getInput('topic_id'); ?>
+                    </div>
+                </div>
+                <div class="control-group">
+                    <div class="control-label">
+                        <?php echo $this->form->getLabel('title'); ?>
+                    </div>
+                    <div class="controls">
+                        <?php echo $this->form->getInput('title'); ?>
+                    </div>
+                </div>
+                <div class="control-group">
+                    <div class="control-label">
+                        <?php echo $this->form->getLabel('content'); ?>
+                    </div>
+                    <div class="controls">
+                        <?php echo $this->form->getInput('content'); ?>
+                    </div>
+                </div>
+                <button type="submit" class="btn btn-primary"><?php echo JText::_('JSUBMIT'); ?></button>
+                <input type="hidden" name="task" value="kunenatopic2article.save" />
+                <?php echo JHtml::_('form.token'); ?>
+            </fieldset>
+        </div>
+    </div>
+</form>
