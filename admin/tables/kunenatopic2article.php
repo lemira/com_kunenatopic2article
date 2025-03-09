01f17824844b6b3753f382538e9c5a88760b8e07
lemira <lemirat@gmail.com>
2025-03-09 15:22:22 +0100

    Create kunenatopic2article.php
    Создание таблицы

---
+++ admin/tables/kunenatopic2article.php
@@ -0,0 +1,10 @@
+<?php
+defined('_JEXEC') or die('Restricted access');
+
+class KunenaTopic2ArticleTableKunenaTopic2Article extends JTable
+{
+    public function __construct(&$db)
+    {
+        parent::__construct('#__kunena_topic2article', 'id', $db);
+    }
+}
