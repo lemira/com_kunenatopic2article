19c54b7b74d30d58f8747dc7b942a475f1095f86
lemira <lemirat@gmail.com>
2025-03-10 18:48:54 +0100

    Update controller.php
    отладка

--- admin/controller.php
+++ admin/controller.php
@@ -11,7 +11,14 @@ class KunenaTopic2ArticleController extends JControllerLegacy
 {
     public function display($cachable = false, $urlparams = false)
     {
-        $this->setRedirect('index.php?option=com_kunenatopic2article');
+        $logFile = JPATH_BASE . '/logs/controller_debug.log';
+        $message = "Displaying view in KunenaTopic2ArticleController at " . date('Y-m-d H:i:s') . "\n";
+        file_put_contents($logFile, $message, FILE_APPEND);
+
+        $view = $this->getView('Topics', 'html');
+        $view->setModel($this->getModel('Topics'));
+        $view->display();
+
         return $this;
     }
 }
