1d818a2e48afdd2e69a1907b38ff29046db9ede6
lemira <lemirat@gmail.com>
2025-03-10 20:49:41 +0100

    Update controller.php
    с дополнительной отладкой

--- admin/controller.php
+++ admin/controller.php
@@ -5,19 +5,34 @@ jimport('joomla.application.component.controller');
 
 $logFile = JPATH_BASE . '/logs/controller_debug.log';
 $message = "Loading KunenaTopic2ArticleController at " . date('Y-m-d H:i:s') . "\n";
-file_put_contents($logFile, $message, FILE_APPEND);
+file_put_contents($logFile, $message, FILE_APPEND | FILE_IGNORE_NEW_LINES | FILE_APPEND_ERRORS);
 
 class KunenaTopic2ArticleController extends JControllerLegacy
 {
+    public function __construct($config = array())
+    {
+        parent::__construct($config);
+
+        $logFile = JPATH_BASE . '/logs/controller_debug.log';
+        $message = "Constructing KunenaTopic2ArticleController at " . date('Y-m-d H:i:s') . "\n";
+        file_put_contents($logFile, $message, FILE_APPEND | FILE_IGNORE_NEW_LINES | FILE_APPEND_ERRORS);
+    }
+
     public function display($cachable = false, $urlparams = false)
     {
         $logFile = JPATH_BASE . '/logs/controller_debug.log';
         $message = "Displaying view in KunenaTopic2ArticleController at " . date('Y-m-d H:i:s') . "\n";
-        file_put_contents($logFile, $message, FILE_APPEND);
+        file_put_contents($logFile, $message, FILE_APPEND | FILE_IGNORE_NEW_LINES | FILE_APPEND_ERRORS);
 
-        $view = $this->getView('Topics', 'html');
-        $view->setModel($this->getModel('Topics'));
-        $view->display();
+        try {
+            $view = $this->getView('Topics', 'html');
+            $model = $this->getModel('Topics');
+            $view->setModel($model, true);
+            $view->display();
+        } catch (Exception $e) {
+            $message = "Error in display: " . $e->getMessage() . " at " . date('Y-m-d H:i:s') . "\n";
+            file_put_contents($logFile, $message, FILE_APPEND | FILE_IGNORE_NEW_LINES | FILE_APPEND_ERRORS);
+        }
 
         return $this;
     }
