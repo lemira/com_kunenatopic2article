12fc92a9e77e9df69140aa2416826b29c51dcc1f
lemira <lemirat@gmail.com>
2025-03-11 00:08:31 +0100

    Update controller.php
    Решение Проблемы с Константой FILE_APPEND_ERRORS

--- admin/controller.php
+++ admin/controller.php
@@ -4,8 +4,11 @@ defined('_JEXEC') or die('Restricted access');
 jimport('joomla.application.component.controller');
 
 $logFile = JPATH_BASE . '/logs/controller_debug.log';
+if (!file_exists(dirname($logFile))) {
+    mkdir(dirname($logFile), 0755, true);
+}
 $message = "Loading KunenaTopic2ArticleController at " . date('Y-m-d H:i:s') . "\n";
-file_put_contents($logFile, $message, FILE_APPEND | FILE_IGNORE_NEW_LINES | FILE_APPEND_ERRORS);
+file_put_contents($logFile, $message, FILE_APPEND | FILE_IGNORE_NEW_LINES);
 
 class KunenaTopic2ArticleController extends JControllerLegacy
 {
@@ -15,14 +18,14 @@ class KunenaTopic2ArticleController extends JControllerLegacy
 
         $logFile = JPATH_BASE . '/logs/controller_debug.log';
         $message = "Constructing KunenaTopic2ArticleController at " . date('Y-m-d H:i:s') . "\n";
-        file_put_contents($logFile, $message, FILE_APPEND | FILE_IGNORE_NEW_LINES | FILE_APPEND_ERRORS);
+        file_put_contents($logFile, $message, FILE_APPEND | FILE_IGNORE_NEW_LINES);
     }
 
     public function display($cachable = false, $urlparams = false)
     {
         $logFile = JPATH_BASE . '/logs/controller_debug.log';
         $message = "Displaying view in KunenaTopic2ArticleController at " . date('Y-m-d H:i:s') . "\n";
-        file_put_contents($logFile, $message, FILE_APPEND | FILE_IGNORE_NEW_LINES | FILE_APPEND_ERRORS);
+        file_put_contents($logFile, $message, FILE_APPEND | FILE_IGNORE_NEW_LINES);
 
         try {
             $view = $this->getView('Topics', 'html');
@@ -31,7 +34,7 @@ class KunenaTopic2ArticleController extends JControllerLegacy
             $view->display();
         } catch (Exception $e) {
             $message = "Error in display: " . $e->getMessage() . " at " . date('Y-m-d H:i:s') . "\n";
-            file_put_contents($logFile, $message, FILE_APPEND | FILE_IGNORE_NEW_LINES | FILE_APPEND_ERRORS);
+            file_put_contents($logFile, $message, FILE_APPEND | FILE_IGNORE_NEW_LINES);
         }
 
         return $this;
