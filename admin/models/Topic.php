fc049edc17623c87262c01762bad810f26286c77
lemira <lemirat@gmail.com>
2025-03-10 20:51:12 +0100

    Update Topic.php
    Обновление 
    Добавлены флаги FILE_APPEND_ERRORS для лучшего логирования.

--- admin/models/Topic.php
+++ admin/models/Topic.php
@@ -9,15 +9,17 @@ class KunenaTopic2ArticleModelTopics extends JModelForm
     {
         $logFile = JPATH_BASE . '/logs/model_debug.log';
         $message = "Loading form in KunenaTopic2ArticleModelTopics at " . date('Y-m-d H:i:s') . "\n";
-        file_put_contents($logFile, $message, FILE_APPEND);
+        file_put_contents($logFile, $message, FILE_APPEND | FILE_IGNORE_NEW_LINES | FILE_APPEND_ERRORS);
 
-        // Указываем путь к XML-форме
         JForm::addFormPath(JPATH_COMPONENT . '/models/forms');
         $form = $this->loadForm('com_kunenatopic2article.topics', 'topics', array('control' => 'jform', 'load_data' => $loadData));
 
         if (empty($form)) {
             $message = "Form loading failed in KunenaTopic2ArticleModelTopics at " . date('Y-m-d H:i:s') . "\n";
-            file_put_contents($logFile, $message, FILE_APPEND);
+            file_put_contents($logFile, $message, FILE_APPEND | FILE_IGNORE_NEW_LINES | FILE_APPEND_ERRORS);
+        } else {
+            $message = "Form loaded successfully in KunenaTopic2ArticleModelTopics at " . date('Y-m-d H:i:s') . "\n";
+            file_put_contents($logFile, $message, FILE_APPEND | FILE_IGNORE_NEW_LINES | FILE_APPEND_ERRORS);
         }
 
         return $form;
