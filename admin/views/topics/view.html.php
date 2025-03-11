959e065f9321b16cee6ecf17442176ae8971bc4c
lemira <lemirat@gmail.com>
2025-03-11 16:43:37 +0100

    Update view.html.php
    Обновление представления

--- admin/views/topics/view.html.php
+++ admin/views/topics/view.html.php
@@ -7,12 +7,7 @@ class KunenaTopic2ArticleViewTopics extends JViewLegacy
 {
     public function display($tpl = null)
     {
-        $logFile = JPATH_BASE . '/logs/view_debug.log';
-        $message = "Loading KunenaTopic2ArticleViewTopics at " . date('Y-m-d H:i:s') . "\n";
-        file_put_contents($logFile, $message, FILE_APPEND);
-
-        $model = $this->getModel();
-        $this->form = $model->getForm();
+        $this->items = $this->get('Items');
 
         parent::display($tpl);
     }
