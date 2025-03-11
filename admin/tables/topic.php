27492969ce9bf12c133772b171d4be76a73a9030
lemira <lemirat@gmail.com>
2025-03-11 23:36:41 +0100

    Update topic.php

--- admin/tables/topic.php
+++ admin/tables/topic.php
@@ -1,7 +1,22 @@
+<?php
 defined('_JEXEC') or die;
 
-class YourComponentTableTopic extends JTable
+class KunenaTopic2ArticleTableTopic extends JTable
 {
+    public $id = null;
+    public $topic_selection = null;
+    public $article_category = null;
+    public $post_transfer_scheme = null;
+    public $max_article_size = null;
+    public $post_author = null;
+    public $post_creation_date = null;
+    public $post_creation_time = null;
+    public $post_ids = null;
+    public $post_title = null;
+    public $kunena_post_link = null;
+    public $reminder_lines = null;
+    public $ignored_authors = null;
+
     public function __construct(&$db)
     {
         parent::__construct('#__kunenatopic2article_params', 'id', $db);
