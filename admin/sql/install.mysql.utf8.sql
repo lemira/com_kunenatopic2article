000047dee2e53fb17fd4da1bad166c4c9b043117
lemira <lemirat@gmail.com>
2025-03-11 23:00:41 +0100

    Update install.mysql.utf8.sql
    SQL-скрипт для создания таблицы

--- admin/sql/install.mysql.utf8.sql
+++ admin/sql/install.mysql.utf8.sql
@@ -1,1 +1,17 @@
 -- Install script for com_kunenatopic2article
+CREATE TABLE IF NOT EXISTS `#__kunenatopic2article_params` (
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `topic_selection` INT NOT NULL DEFAULT 0,
+    `article_category` INT NOT NULL DEFAULT 0,
+    `post_transfer_scheme` TINYINT(1) NOT NULL DEFAULT 1,
+    `max_article_size` INT NOT NULL DEFAULT 40000,
+    `post_author` TINYINT(1) NOT NULL DEFAULT 1,
+    `post_creation_date` TINYINT(1) NOT NULL DEFAULT 1,
+    `post_creation_time` TINYINT(1) NOT NULL DEFAULT 0,
+    `post_ids` TINYINT(1) NOT NULL DEFAULT 1,
+    `post_title` TINYINT(1) NOT NULL DEFAULT 0,
+    `kunena_post_link` TINYINT(1) NOT NULL DEFAULT 0,
+    `reminder_lines` INT NOT NULL DEFAULT 0,
+    `ignored_authors` TEXT DEFAULT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
