8e86c3937eb2561d5de01a8cc84e15708fbf2972
lemira <lemirat@gmail.com>
2025-03-12 18:04:08 +0100

    Update view.html.php
    Обновляем представление

--- admin/views/topics/view.html.php
+++ admin/views/topics/view.html.php
@@ -3,13 +3,24 @@ defined('_JEXEC') or die;
 
 class KunenaTopic2ArticleViewTopics extends JViewLegacy
 {
+    protected $state;
+
     public function display($tpl = null)
     {
         $model = $this->getModel();
         $this->params = $model->getParams();
+        $this->state = $model->getState();
         
         if (!$this->params) {
             JFactory::getApplication()->enqueueMessage('No parameters found in database', 'warning');
+        } else {
+            JFactory::getApplication()->enqueueMessage('Parameters loaded successfully: ' . print_r($this->params, true), 'message');
+        }
+
+        if (!$this->state) {
+            JFactory::getApplication()->enqueueMessage('State not loaded', 'warning');
+        } else {
+            JFactory::getApplication()->enqueueMessage('State loaded: ' . print_r($this->state, true), 'message');
         }
         
         parent::display($tpl);
