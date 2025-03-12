c464dc0e924c00ef7e62552e2cd9a7d466ea198c
lemira <lemirat@gmail.com>
2025-03-12 17:23:51 +0100

    Update Topic.php
    вызываем метод getParams()

--- admin/models/Topic.php
+++ admin/models/Topic.php
@@ -1,7 +1,5 @@
 <?php
-defined('_JEXEC') or die('Restricted access');
-
-jimport('joomla.application.component.modeladmin');
+defined('_JEXEC') or die;
 
 class KunenaTopic2ArticleModelTopic extends JModelAdmin
 {
@@ -10,43 +8,13 @@ class KunenaTopic2ArticleModelTopic extends JModelAdmin
         return JTable::getInstance($type, $prefix, $config);
     }
 
-    public function getItems()
-    {
-        $db = JFactory::getDbo();
-        $query = $db->getQuery(true)
-            ->select('*')
-            ->from('#__kunenatopic2article');
-        $db->setQuery($query);
-        $items = $db->loadObjectList();
-
-        // Обнуляем Topic ID для отображения
-        foreach ($items as &$item) {
-            $item->topic_id = 0; // Обнуляем Topic ID
-        }
-
-        return $items;
-    }
-
     public function getForm($data = array(), $loadData = true)
     {
-        $logFile = JPATH_BASE . '/administrator/logs/model_debug.log';
-        if (!file_exists(dirname($logFile))) {
-            mkdir(dirname($logFile), 0755, true);
-        }
-        $message = "Loading form in KunenaTopic2ArticleModelTopic at " . date('Y-m-d H:i:s') . "\n";
-        file_put_contents($logFile, $message, FILE_APPEND | FILE_IGNORE_NEW_LINES);
-
-        JForm::addFormPath(JPATH_COMPONENT . '/models/forms');
         $form = $this->loadForm('com_kunenatopic2article.topic', 'topic', array('control' => 'jform', 'load_data' => $loadData));
-
         if (empty($form)) {
-            $message = "Form loading failed in KunenaTopic2ArticleModelTopic at " . date('Y-m-d H:i:s') . "\n";
-            file_put_contents($logFile, $message, FILE_APPEND | FILE_IGNORE_NEW_LINES);
-        } else {
-            $message = "Form loaded successfully in KunenaTopic2ArticleModelTopic at " . date('Y-m-d H:i:s') . "\n";
-            file_put_contents($logFile, $message, FILE_APPEND | FILE_IGNORE_NEW_LINES);
+            JFactory::getApplication()->enqueueMessage('Form not found', 'error');
+            return false;
         }
-
         return $form;
     }
 
@@ -57,5 +25,20 @@ class KunenaTopic2ArticleModelTopic extends JModelAdmin
             $data = $this->getItem();
         }
         return $data;
+    }
+
+    public function getParams()
+    {
+        $db = JFactory::getDbo();
+        $query = $db->getQuery(true);
+        $query->select('*')
+              ->from($db->quoteName('#__kunenatopic2article_params'))
+              ->where($db->quoteName('id') . ' = 1');
+        $db->setQuery($query);
+        $result = $db->loadObject();
+        if (!$result) {
+            JFactory::getApplication()->enqueueMessage('No params found in database', 'warning');
+        }
+        return $result;
     }
 }
