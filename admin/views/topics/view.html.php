503d3682ba367f1e5ae8b9326450255e2d2f1236
lemira <lemirat@gmail.com>
2025-03-12 17:00:56 +0100

    Update view.html.php

--- admin/views/topics/view.html.php
+++ admin/views/topics/view.html.php
@@ -1,14 +1,17 @@
 <?php
-defined('_JEXEC') or die('Restricted access');
-
-jimport('joomla.application.component.view');
+defined('_JEXEC') or die;
 
 class KunenaTopic2ArticleViewTopics extends JViewLegacy
 {
     public function display($tpl = null)
     {
-        $this->items = $this->get('Items');
-
+        $model = $this->getModel();
+        $this->params = $model->getParams();
+        
+        if (!$this->params) {
+            JFactory::getApplication()->enqueueMessage('No parameters found in database', 'warning');
+        }
+        
         parent::display($tpl);
     }
 }
