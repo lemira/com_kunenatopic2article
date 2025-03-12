f700174a939fe263febb5b15b75b114441f86a64
lemira <lemirat@gmail.com>
2025-03-12 15:18:54 +0100

    Update kunenatopic2article.php
     вывод ошибок в точку входа админки

--- admin/kunenatopic2article.php
+++ admin/kunenatopic2article.php
@@ -1,14 +1,9 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 defined('_JEXEC') or die('Restricted access');
-
-jimport('joomla.application.component.controller');
-
-$logFile = JPATH_BASE . '/logs/controller_debug.log';
-if (!file_exists(dirname($logFile))) {
-    mkdir(dirname($logFile), 0755, true);
-}
-$message = "Loading component at " . date('Y-m-d H:i:s') . "\n";
-file_put_contents($logFile, $message, FILE_APPEND);
 
 $controller = JControllerLegacy::getInstance('KunenaTopic2Article');
 $controller->execute(JFactory::getApplication()->input->get('task'));
