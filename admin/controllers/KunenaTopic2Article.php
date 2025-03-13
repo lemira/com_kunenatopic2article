6377237059a509cf3a09496c9159676880a7fd1d
lemira <lemirat@gmail.com>
2025-03-13 23:44:04 +0100

    Update KunenaTopic2Article.php
    больше отладки

--- admin/controllers/KunenaTopic2Article.php
+++ admin/controllers/KunenaTopic2Article.php
@@ -3,6 +3,12 @@ defined('_JEXEC') or die;
 
 class KunenaTopic2ArticleController extends JControllerLegacy
 {
+    public function __construct($config = array())
+    {
+        parent::__construct($config);
+        JFactory::getApplication()->enqueueMessage('Controller KunenaTopic2ArticleController initialized', 'message');
+    }
+
     public function display($cachable = false, $urlparams = false)
     {
         JFactory::getApplication()->enqueueMessage('Display called', 'message');
