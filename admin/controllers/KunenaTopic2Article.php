935baf550edffdb726913e790756f314fd1993a8
lemira <lemirat@gmail.com>
2025-03-13 21:45:57 +0100

    Update KunenaTopic2Article.php
    Проверяем контроллер

--- admin/controllers/KunenaTopic2Article.php
+++ admin/controllers/KunenaTopic2Article.php
@@ -5,6 +5,7 @@ class KunenaTopic2ArticleController extends JControllerLegacy
 {
     public function display($cachable = false, $urlparams = false)
     {
+        JFactory::getApplication()->enqueueMessage('Display called', 'message');
         $this->setRedirect('index.php?option=com_kunenatopic2article&view=topics');
         return parent::display($cachable, $urlparams);
     }
@@ -15,7 +16,7 @@ class KunenaTopic2ArticleController extends JControllerLegacy
         $model = $this->getModel('Topic', 'KunenaTopic2ArticleModel');
         $data = $app->input->get('jform', array(), 'array');
         
-        JFactory::getApplication()->enqueueMessage('Save task called. Data: ' . print_r($data, true), 'message');
+        $app->enqueueMessage('Save task called. Data: ' . print_r($data, true), 'message');
         
         if ($model->save($data)) {
             $app->enqueueMessage('Parameters saved successfully', 'success');
@@ -31,7 +32,7 @@ class KunenaTopic2ArticleController extends JControllerLegacy
         $app = JFactory::getApplication();
         $model = $this->getModel('Topic', 'KunenaTopic2ArticleModel');
         
-        JFactory::getApplication()->enqueueMessage('Reset task called', 'message');
+        $app->enqueueMessage('Reset task called', 'message');
         
         if ($model->reset()) {
             $app->enqueueMessage('Parameters reset to default values', 'success');
