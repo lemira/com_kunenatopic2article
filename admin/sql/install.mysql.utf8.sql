5cb33ba5301e9a795ba35ce5eb80658d84ddf676
lemira <lemirat@gmail.com>
2025-03-13 13:23:54 +0100

    Update install.mysql.utf8.sql
     Убираем INSERT IGNORE из SQL

--- admin/sql/install.mysql.utf8.sql
+++ admin/sql/install.mysql.utf8.sql
@@ -14,11 +14,3 @@ CREATE TABLE IF NOT EXISTS `#__kunenatopic2article_params` (
     `ignored_authors` TEXT DEFAULT NULL,
     PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
-INSERT IGNORE INTO `#__kunenatopic2article_params` (
-    `topic_selection`, `article_category`, `post_transfer_scheme`, `max_article_size`,
-    `post_author`, `post_creation_date`, `post_creation_time`, `post_ids`,
-    `post_title`, `kunena_post_link`, `reminder_lines`, `ignored_authors`
-) VALUES (
-    0, 0, 1, 40000, 1, NOW(), NOW(), 1, 0, 0, 0, NULL
-);
