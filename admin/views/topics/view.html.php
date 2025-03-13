4971a75d0edc39cd46f5861db59bcb9a6bd4d626
lemira <lemirat@gmail.com>
2025-03-13 19:11:15 +0100

    Update view.html.php
    Убираем отладочные сообщения

--- admin/views/topics/view.html.php
+++ admin/views/topics/view.html.php
@@ -11,18 +11,6 @@ class KunenaTopic2ArticleViewTopics extends JViewLegacy
         $this->params = $model->getParams();
         $this->state = $model->getState();
         
-        if (!$this->params) {
-            JFactory::getApplication()->enqueueMessage('No parameters found in database', 'warning');
-        } else {
-            JFactory::getApplication()->enqueueMessage('Parameters loaded successfully: ' . print_r($this->params, true), 'message');
-        }
-
-        if (!$this->state) {
-            JFactory::getApplication()->enqueueMessage('State not loaded', 'warning');
-        } else {
-            JFactory::getApplication()->enqueueMessage('State loaded: ' . print_r($this->state, true), 'message');
-        }
-        
         parent::display($tpl);
     }
 }
