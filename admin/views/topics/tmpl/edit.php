ae7a4d9da28d5ddc94379ca928218dbdf05b70c2
lemira <lemirat@gmail.com>
2025-03-13 20:00:07 +0100

    Create edit.php
    Создаём шаблон формы редактирования

---
+++ admin/views/topics/tmpl/edit.php
@@ -0,0 +1,23 @@
+<?php
+defined('_JEXEC') or die;
+
+JHtml::_('behavior.formvalidator');
+JHtml::_('formbehavior.chosen', 'select');
+?>
+
+<form action="<?php echo JRoute::_('index.php?option=com_kunenatopic2article'); ?>" method="post" name="adminForm" id="adminForm" class="form-validate">
+    <div class="container-fluid">
+        <h1><?php echo JText::_('COM_KUNENATOPIC2ARTICLE_PARAMS_TITLE'); ?></h1>
+        
+        <?php echo $this->form->renderFieldset('article_params'); ?>
+        <?php echo $this->form->renderFieldset('post_info'); ?>
+        
+        <div class="btn-toolbar mt-3">
+            <button type="submit" class="btn btn-success"><?php echo JText::_('JSUBMIT'); ?></button>
+            <a href="<?php echo JRoute::_('index.php?option=com_kunenatopic2article'); ?>" class="btn btn-secondary"><?php echo JText::_('JCANCEL'); ?></a>
+        </div>
+        
+        <input type="hidden" name="task" value="save" />
+        <?php echo JHtml::_('form.token'); ?>
+    </div>
+</form>
