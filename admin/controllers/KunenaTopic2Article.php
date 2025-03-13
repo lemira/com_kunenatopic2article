06ddc6f05eb9b05064a7e49aa388cae8f2f89a44
lemira <lemirat@gmail.com>
2025-03-13 21:34:59 +0100

    Update KunenaTopic2Article.php
     Добавляем отладку в контроллер

--- admin/controllers/KunenaTopic2Article.php
+++ admin/controllers/KunenaTopic2Article.php
@@ -15,8 +15,12 @@ class KunenaTopic2ArticleController extends JControllerLegacy
         $model = $this->getModel('Topic', 'KunenaTopic2ArticleModel');
         $data = $app->input->get('jform', array(), 'array');
         
+        JFactory::getApplication()->enqueueMessage('Save task called. Data: ' . print_r($data, true), 'message');
+        
         if ($model->save($data)) {
             $app->enqueueMessage('Parameters saved successfully', 'success');
+        } else {
+            $app->enqueueMessage('Failed to save parameters', 'error');
         }
         
         $this->setRedirect('index.php?option=com_kunenatopic2article&view=topics');
@@ -27,8 +31,12 @@ class KunenaTopic2ArticleController extends JControllerLegacy
         $app = JFactory::getApplication();
         $model = $this->getModel('Topic', 'KunenaTopic2ArticleModel');
         
+        JFactory::getApplication()->enqueueMessage('Reset task called', 'message');
+        
         if ($model->reset()) {
             $app->enqueueMessage('Parameters reset to default values', 'success');
+        } else {
+            $app->enqueueMessage('Failed to reset parameters', 'error');
         }
         
         $this->setRedirect('index.php?option=com_kunenatopic2article&view=topics');
