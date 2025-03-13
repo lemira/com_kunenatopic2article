ec8efd560ba91d067eb9228927f82d7ea35bbf2c
lemira <lemirat@gmail.com>
2025-03-13 20:35:17 +0100

    Update view.html.php
    Проверяем представление

--- admin/views/topics/view.html.php
+++ admin/views/topics/view.html.php
@@ -4,12 +4,22 @@ defined('_JEXEC') or die;
 class KunenaTopic2ArticleViewTopics extends JViewLegacy
 {
     protected $state;
+    protected $form;
 
     public function display($tpl = null)
     {
         $model = $this->getModel();
         $this->params = $model->getParams();
         $this->state = $model->getState();
+        
+        // Если layout=edit, подключаем форму
+        $layout = $this->getLayout();
+        if ($layout == 'edit') {
+            $this->form = $model->getForm();
+            if (!$this->form) {
+                JFactory::getApplication()->enqueueMessage('Form failed to load', 'error');
+            }
+        }
         
         parent::display($tpl);
     }
