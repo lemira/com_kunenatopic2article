67a35554e472f2339058289cae3aca2039843502
lemira <lemirat@gmail.com>
2025-03-13 20:12:08 +0100

    Update KunenaTopic2Article.php
    Обновляем контроллер

--- admin/controllers/KunenaTopic2Article.php
+++ admin/controllers/KunenaTopic2Article.php
@@ -1,18 +1,47 @@
 <?php
 defined('_JEXEC') or die;
 
-class KunenaTopic2ArticleController extends JControllerForm
+class KunenaTopic2ArticleController extends JControllerLegacy
 {
-    public function __construct($config = array())
-    {
-        parent::__construct($config);
-    }
-
     public function display($cachable = false, $urlparams = false)
     {
-        $view = $this->getView('topics', 'html');
-        $model = $this->getModel('kunenatopic2article');
-        $view->setModel($model, true);
-        $view->display();
+        $this->setRedirect('index.php?option=com_kunenatopic2article&view=topics');
+        return parent::display($cachable, $urlparams);
+    }
+
+    public function edit()
+    {
+        $this->setRedirect('index.php?option=com_kunenatopic2article&view=topics&layout=edit');
+    }
+
+    public function save()
+    {
+        $app = JFactory::getApplication();
+        $model = $this->getModel('Topic', 'KunenaTopic2ArticleModel');
+        $data = $app->input->get('jform', array(), 'array');
+        
+        if ($model->save($data)) {
+            $app->enqueueMessage('Parameters saved successfully', 'success');
+        }
+        
+        $this->setRedirect('index.php?option=com_kunenatopic2article&view=topics');
+    }
+
+    public function reset()
+    {
+        $app = JFactory::getApplication();
+        $model = $this->getModel('Topic', 'KunenaTopic2ArticleModel');
+        
+        if ($model->reset()) {
+            $app->enqueueMessage('Parameters reset to default values', 'success');
+        }
+        
+        $this->setRedirect('index.php?option=com_kunenatopic2article&view=topics');
+    }
+
+    public function create()
+    {
+        JFactory::getApplication()->enqueueMessage('Article creation not implemented yet', 'warning');
+        $this->setRedirect('index.php?option=com_kunenatopic2article&view=topics');
     }
 }
