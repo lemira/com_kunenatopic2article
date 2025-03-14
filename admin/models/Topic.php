fd145c6f9ac91191899dc9cd7eca6b40317b43bd
lemira <lemirat@gmail.com>
2025-03-14 15:24:12 +0100

    Update Topic.php
    обновим topic.php, чтобы он корректно работал с новой структурой таблицы

--- admin/models/Topic.php
+++ admin/models/Topic.php
@@ -3,8 +3,6 @@ defined('_JEXEC') or die;
 
 class KunenaTopic2ArticleModelTopic extends JModelAdmin
 {
-    protected $state;
-
     public function getTable($type = 'Topic', $prefix = 'KunenaTopic2ArticleTable', $config = array())
     {
         return JTable::getInstance($type, $prefix, $config);
@@ -12,24 +10,19 @@ class KunenaTopic2ArticleModelTopic extends JModelAdmin
 
     public function getForm($data = array(), $loadData = true)
     {
-        $form = $this->loadForm('com_kunenatopic2article.params', 'params', array('control' => 'jform', 'load_data' => $loadData));
+        $form = $this->loadForm('com_kunenatopic2article.topic', 'topic', array('control' => 'jform', 'load_data' => $loadData));
         if (empty($form)) {
-            JFactory::getApplication()->enqueueMessage('Form not found', 'error');
             return false;
         }
-        
-        if ($loadData) {
-            $data = $this->loadFormData();
-            $data->topic_selection = 0;
-            $form->bind($data);
-        }
-        
         return $form;
     }
 
     protected function loadFormData()
     {
-        $data = $this->getParams();
+        $data = JFactory::getApplication()->getUserState('com_kunenatopic2article.edit.topic.data', array());
+        if (empty($data)) {
+            $data = $this->getParams();
+        }
         return $data;
     }
 
@@ -38,108 +31,72 @@ class KunenaTopic2ArticleModelTopic extends JModelAdmin
         $db = JFactory::getDbo();
         $query = $db->getQuery(true);
         $query->select('*')
-              ->from($db->quoteName('#__kunenatopic2article_params'))
-              ->where($db->quoteName('id') . ' = 1');
+              ->from('#__kunenatopic2article_params')
+              ->where('id = 1');
         $db->setQuery($query);
-        $result = $db->loadObject();
-        if (!$result) {
-            JFactory::getApplication()->enqueueMessage('No params found in database', 'warning');
+        $row = $db->loadAssoc();
+
+        if (empty($row)) {
+            return [
+                'topic_selection' => 0,
+                'article_category' => 0,
+                'post_transfer_scheme' => 1,
+                'max_article_size' => 40000,
+                'post_author' => 1,
+                'post_creation_date' => date('Y-m-d H:i:s'),
+                'post_creation_time' => date('Y-m-d H:i:s'),
+                'post_ids' => 1,
+                'post_title' => 0,
+                'kunena_post_link' => 0,
+                'reminder_lines' => 0,
+                'ignored_authors' => ''
+            ];
         }
-        return $result;
+
+        return $row;
     }
 
     public function save($data)
     {
         $db = JFactory::getDbo();
         $query = $db->getQuery(true);
-        
-        JFactory::getApplication()->enqueueMessage('Saving data: ' . print_r($data, true), 'message');
-        
-        $fields = array(
-            $db->quoteName('topic_selection') . ' = ' . (int)$data['topic_selection'],
-            $db->quoteName('article_category') . ' = ' . (int)$data['article_category'],
-            $db->quoteName('post_transfer_scheme') . ' = ' . (int)$data['post_transfer_scheme'],
-            $db->quoteName('max_article_size') . ' = ' . (int)$data['max_article_size'],
-            $db->quoteName('post_author') . ' = ' . (int)$data['post_author'],
-            $db->quoteName('post_creation_date') . ' = ' . $db->quote($data['post_creation_date']),
-            $db->quoteName('post_creation_time') . ' = ' . $db->quote($data['post_creation_time']),
-            $db->quoteName('post_ids') . ' = ' . (int)$data['post_ids'],
-            $db->quoteName('post_title') . ' = ' . (int)$data['post_title'],
-            $db->quoteName('kunena_post_link') . ' = ' . (int)$data['kunena_post_link'],
-            $db->quoteName('reminder_lines') . ' = ' . (int)$data['reminder_lines'],
-            $db->quoteName('ignored_authors') . ' = ' . $db->quote($data['ignored_authors'])
-        );
-        
-        $query->update($db->quoteName('#__kunenatopic2article_params'))
-              ->set($fields)
-              ->where($db->quoteName('id') . ' = 1');
-        
+        $query->update('#__kunenatopic2article_params')
+              ->set('topic_selection = ' . $db->quote($data['topic_selection']))
+              ->set('article_category = ' . $db->quote($data['article_category']))
+              ->set('post_transfer_scheme = ' . $db->quote($data['post_transfer_scheme']))
+              ->set('max_article_size = ' . $db->quote($data['max_article_size']))
+              ->set('post_author = ' . $db->quote($data['post_author']))
+              ->set('post_creation_date = ' . $db->quote($data['post_creation_date']))
+              ->set('post_creation_time = ' . $db->quote($data['post_creation_time']))
+              ->set('post_ids = ' . $db->quote($data['post_ids']))
+              ->set('post_title = ' . $db->quote($data['post_title']))
+              ->set('kunena_post_link = ' . $db->quote($data['kunena_post_link']))
+              ->set('reminder_lines = ' . $db->quote($data['reminder_lines']))
+              ->set('ignored_authors = ' . $db->quote($data['ignored_authors']))
+              ->where('id = 1');
         $db->setQuery($query);
-        
-        JFactory::getApplication()->enqueueMessage('SQL Query: ' . (string)$query, 'message');
-        
-        try {
-            $db->execute();
-            JFactory::getApplication()->enqueueMessage('Database updated successfully', 'message');
-            return true;
-        } catch (Exception $e) {
-            JFactory::getApplication()->enqueueMessage('Error saving params: ' . $e->getMessage(), 'error');
-            return false;
-        }
+        return $db->execute();
     }
 
     public function reset()
     {
         $db = JFactory::getDbo();
         $query = $db->getQuery(true);
-        
-        $fields = array(
-            $db->quoteName('topic_selection') . ' = 0',
-            $db->quoteName('article_category') . ' = 0',
-            $db->quoteName('post_transfer_scheme') . ' = 1',
-            $db->quoteName('max_article_size') . ' = 40000',
-            $db->quoteName('post_author') . ' = 1',
-            $db->quoteName('post_creation_date') . ' = ' . $db->quote(date('Y-m-d H:i:s')),
-            $db->quoteName('post_creation_time') . ' = ' . $db->quote(date('Y-m-d H:i:s')),
-            $db->quoteName('post_ids') . ' = 1',
-            $db->quoteName('post_title') . ' = 0',
-            $db->quoteName('kunena_post_link') . ' = 0',
-            $db->quoteName('reminder_lines') . ' = 0',
-            $db->quoteName('ignored_authors') . ' = NULL'
-        );
-        
-        $query->update($db->quoteName('#__kunenatopic2article_params'))
-              ->set($fields)
-              ->where($db->quoteName('id') . ' = 1');
-        
+        $query->update('#__kunenatopic2article_params')
+              ->set('topic_selection = 0')
+              ->set('article_category = 0')
+              ->set('post_transfer_scheme = 1')
+              ->set('max_article_size = 40000')
+              ->set('post_author = 1')
+              ->set('post_creation_date = ' . $db->quote(date('Y-m-d H:i:s')))
+              ->set('post_creation_time = ' . $db->quote(date('Y-m-d H:i:s')))
+              ->set('post_ids = 1')
+              ->set('post_title = 0')
+              ->set('kunena_post_link = 0')
+              ->set('reminder_lines = 0')
+              ->set('ignored_authors = NULL')
+              ->where('id = 1');
         $db->setQuery($query);
-        
-        try {
-            $db->execute();
-            JFactory::getApplication()->enqueueMessage('Database reset successfully', 'message');
-            return true;
-        } catch (Exception $e) {
-            JFactory::getApplication()->enqueueMessage('Error resetting params: ' . $e->getMessage(), 'error');
-            return false;
-        }
-    }
-
-    protected function populateState()
-    {
-        $app = JFactory::getApplication();
-        $ordering = $app->input->get('filter_order', 'id');
-        $direction = $app->input->get('filter_order_Dir', 'asc');
-        
-        $this->setState('list.ordering', $ordering);
-        $this->setState('list.direction', $direction);
-    }
-
-    public function getState($property = null, $default = null)
-    {
-        if (!$this->state) {
-            $this->state = new JObject();
-            $this->populateState();
-        }
-        return parent::getState($property, $default);
+        return $db->execute();
     }
 }
