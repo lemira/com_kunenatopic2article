f16bbd62175587296458ce45380d46584e45c0c6
lemira <lemirat@gmail.com>
2025-03-15 15:37:32 +0100

    Update Params.php
    "Updated Params.php to remove debug messages".

--- admin/tables/Params.php
+++ admin/tables/Params.php
@@ -1,33 +1,10 @@
 <?php
 defined('_JEXEC') or die;
 
-// Простая отладка
-JFactory::getApplication()->enqueueMessage('Params.php is being loaded', 'notice');
-
 class KunenaTopic2ArticleTableParams extends JTable
 {
-    public $id = null;
-    public $topic_selection = null;
-    public $article_category = null;
-    public $post_transfer_scheme = null;
-    public $max_article_size = null;
-    public $post_author = null;
-    public $post_creation_date = null;
-    public $post_creation_time = null;
-    public $post_ids = null;
-    public $post_title = null;
-    public $kunena_post_link = null;
-    public $reminder_lines = null;
-    public $ignored_authors = null;
-
-    public function __construct(&$db)
+    public function __construct($db)
     {
-        JFactory::getApplication()->enqueueMessage('Constructing KunenaTopic2ArticleTableParams', 'notice');
         parent::__construct('#__kunenatopic2article_params', 'id', $db);
-    }
-
-    public function check()
-    {
-        return true;
     }
 }
