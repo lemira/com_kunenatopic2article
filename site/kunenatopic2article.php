bfdf3db0a1d8876b20ed80a38c2bbb29e88b8040
lemira <lemirat@gmail.com>
2025-05-09 20:02:13 +0200

    Delete site/kunenatopic2article.php

--- site/kunenatopic2article.php
+++
@@ -1,9 +0,0 @@
-<?php
-defined('_JEXEC') or die;
-
-use Joomla\CMS\Factory;
-use Joomla\CMS\MVC\Controller\BaseController;
-
-$controller = BaseController::getInstance('KunenaTopic2Article');
-$controller->execute(Factory::getApplication()->input->get('task'));
-$controller->redirect();
