0059972c65bbba9a44de4bb2d24620a1b5859ffe
lemira <lemirat@gmail.com>
2025-05-10 18:18:09 +0200

    Update controller.php
     указывает Joomla, что представление topics — это представление по умолчанию

--- admin/controller.php
+++ admin/controller.php
@@ -1,39 +1,15 @@
 <?php
-defined('_JEXEC') or die('Restricted access');
+defined('_JEXEC') or die;
 
-use Joomla\CMS\Language\Text;
 use Joomla\CMS\MVC\Controller\BaseController;
-use Joomla\CMS\Router\Route;
 
-/**
- * KunenaTopic2Article Component Controller
- *
- * @since  1.0
- */
 class KunenaTopic2ArticleController extends BaseController
 {
     protected $default_view = 'topics';
 
-    /**
-     * Method to display a view.
-     *
-     * @param   boolean  $cachable   If true, the view output will be cached
-     * @param   array    $urlparams  An array of safe URL parameters and their variable types
-     *
-     * @return  KunenaTopic2ArticleController  This object to support chaining.
-     *
-     * @since   1.0
-     */
     public function display($cachable = false, $urlparams = false)
     {
-        $view = $this->input->get('view', 'topics');
-
-        // Если view не указан или не равен 'topics', перенаправляем
-        if (!$view || $view != 'topics') {
-            $this->setRedirect(Route::_('index.php?option=com_kunenatopic2article&view=topics', false));
-            $this->redirect();
-        }
-
-        return parent::display($cachable, $urlparams);
+        $this->setRedirect('index.php?option=com_kunenatopic2article&view=topics');
+        $this->redirect();
     }
 }
