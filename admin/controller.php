1934783509a1804a83848e0367bc00a4af06c05f
lemira <lemirat@gmail.com>
2025-05-10 17:59:32 +0200

    Update controller.php
    добавляем явное перенаправление на view=topics

--- admin/controller.php
+++ admin/controller.php
@@ -1,47 +1,39 @@
 <?php
 defined('_JEXEC') or die('Restricted access');
 
-jimport('joomla.application.component.controller');
+use Joomla\CMS\Language\Text;
+use Joomla\CMS\MVC\Controller\BaseController;
+use Joomla\CMS\Router\Route;
 
-$logFile = JPATH_BASE . '/logs/controller_debug.log';
-if (!file_exists(dirname($logFile))) {
-    mkdir(dirname($logFile), 0755, true);
-}
-$message = "Loading KunenaTopic2ArticleController at " . date('Y-m-d H:i:s') . "\n";
-file_put_contents($logFile, $message, FILE_APPEND | FILE_IGNORE_NEW_LINES);
-
-class KunenaTopic2ArticleController extends JControllerLegacy
+/**
+ * KunenaTopic2Article Component Controller
+ *
+ * @since  1.0
+ */
+class KunenaTopic2ArticleController extends BaseController
 {
-    public function __construct($config = array())
-    {
-        parent::__construct($config);
+    protected $default_view = 'topics';
 
-        $logFile = JPATH_BASE . '/logs/controller_debug.log';
-        $message = "Constructing KunenaTopic2ArticleController at " . date('Y-m-d H:i:s') . "\n";
-        file_put_contents($logFile, $message, FILE_APPEND | FILE_IGNORE_NEW_LINES);
-    }
-
+    /**
+     * Method to display a view.
+     *
+     * @param   boolean  $cachable   If true, the view output will be cached
+     * @param   array    $urlparams  An array of safe URL parameters and their variable types
+     *
+     * @return  KunenaTopic2ArticleController  This object to support chaining.
+     *
+     * @since   1.0
+     */
     public function display($cachable = false, $urlparams = false)
     {
-        $logFile = JPATH_BASE . '/logs/controller_debug.log';
-        $message = "Displaying view in KunenaTopic2ArticleController at " . date('Y-m-d H:i:s') . "\n";
-        file_put_contents($logFile, $message, FILE_APPEND | FILE_IGNORE_NEW_LINES);
+        $view = $this->input->get('view', 'topics');
 
-        try {
-            $view = $this->getView('Topics', 'html');
-            $model = $this->getModel('Topic'); // Изменил с 'Topics' на 'Topic'
-            if ($model) {
-                $view->setModel($model, true);
-                $view->display();
-            } else {
-                $message = "Model 'Topic' not found at " . date('Y-m-d H:i:s') . "\n";
-                file_put_contents($logFile, $message, FILE_APPEND | FILE_IGNORE_NEW_LINES);
-            }
-        } catch (Exception $e) {
-            $message = "Error in display: " . $e->getMessage() . " at " . date('Y-m-d H:i:s') . "\n";
-            file_put_contents($logFile, $message, FILE_APPEND | FILE_IGNORE_NEW_LINES);
+        // Если view не указан или не равен 'topics', перенаправляем
+        if (!$view || $view != 'topics') {
+            $this->setRedirect(Route::_('index.php?option=com_kunenatopic2article&view=topics', false));
+            $this->redirect();
         }
 
-        return $this;
+        return parent::display($cachable, $urlparams);
     }
 }
