a0a26cb6792127f8a5ed7f14e7501c543c978b97
lemira <lemirat@gmail.com>
2025-05-10 19:04:07 +0200

    Update controller.php
    устранение ERR_TOO_MANY_REDIRECTS

--- admin/controller.php
+++ admin/controller.php
@@ -9,7 +9,16 @@ class KunenaTopic2ArticleController extends BaseController
 
     public function display($cachable = false, $urlparams = false)
     {
-        $this->setRedirect('index.php?option=com_kunenatopic2article&view=topics');
-        $this->redirect();
+        // Проверяем, есть ли параметр view в URL
+        $view = $this->input->get('view');
+
+        // Если view отсутствует или не равен 'topics', перенаправляем
+        if (!$view || $view !== 'topics') {
+            $this->setRedirect('index.php?option=com_kunenatopic2article&view=topics');
+            $this->redirect();
+        }
+
+        // Если view=topics, отображаем представление
+        return parent::display($cachable, $urlparams);
     }
 }
