db5473e9948518e16f2584d2efae4cfd6e3e09f7
lemira <lemirat@gmail.com>
2025-05-14 15:59:48 +0200

    Create topic.php
    врем

---
+++ site/models/topic.php
@@ -0,0 +1,12 @@
+<?php
+defined('_JEXEC') or die;
+
+use Joomla\CMS\MVC\Model\BaseDatabaseModel;
+
+class KunenaTopic2ArticleModelTopic extends BaseDatabaseModel
+{
+    public function getTopics()
+    {
+        // Your logic to get topics from Kunena
+    }
+}
