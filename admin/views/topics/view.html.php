d4882a3685a43c87628c34a6590c6e90a06be9e2
lemira <lemirat@gmail.com>
2025-05-14 16:08:38 +0200

    Update view.html.php
    врем

--- admin/views/topics/view.html.php
+++ admin/views/topics/view.html.php
@@ -1,55 +1,22 @@
 <?php
 defined('_JEXEC') or die;
 
-use Joomla\CMS\Factory;
-use Joomla\CMS\HTML\HTMLHelper;
 use Joomla\CMS\Language\Text;
-use Joomla\CMS\Log\Log;
-use Joomla\CMS\MVC\View\HtmlView;
 use Joomla\CMS\Toolbar\ToolbarHelper;
 
-class KunenaTopic2ArticleViewTopics extends HtmlView
+class KunenaTopic2ArticleViewTopics extends JViewLegacy
 {
-    protected $form;
-    protected $item;
-
     public function display($tpl = null)
     {
-        Log::add('Starting display method in KunenaTopic2ArticleViewTopics', Log::DEBUG, 'com_kunenatopic2article');
+        // Загружаем модель
+        $model = $this->getModel();
 
-        // Загружаем форму
-        $this->form = $this->get('Form');
-        if (!$this->form) {
-            Log::add('Failed to load form in KunenaTopic2ArticleViewTopics', Log::ERROR, 'com_kunenatopic2article');
-            throw new Exception(Text::_('Failed to load form'), 500);
-        }
-        Log::add('Form loaded successfully', Log::DEBUG, 'com_kunenatopic2article');
+        // Получаем параметры из модели
+        $this->parameters = $model->getParameters();
 
-        // Загружаем данные
-        $this->item = $this->get('Item');
-        if (!$this->item) {
-            Log::add('Failed to load item in KunenaTopic2ArticleViewTopics', Log::ERROR, 'com_kunenatopic2article');
-            throw new Exception(Text::_('Failed to load item'), 500);
-        }
-        Log::add('Item loaded successfully', Log::DEBUG, 'com_kunenatopic2article');
+        // Устанавливаем заголовок страницы
+        ToolbarHelper::title(Text::_('COM_KUNENATOPIC2ARTICLE_VIEW_DEFAULT_TITLE'), 'stack');
 
-        // Проверяем ошибки
-        if (count($errors = $this->get('Errors'))) {
-            Log::add('Errors found: ' . implode("\n", $errors), Log::ERROR, 'com_kunenatopic2article');
-            throw new Exception(implode("\n", $errors), 500);
-        }
-
-        $this->addToolbar();
-
-        Log::add('Calling parent::display in KunenaTopic2ArticleViewTopics', Log::DEBUG, 'com_kunenatopic2article');
         parent::display($tpl);
-    }
-
-    protected function addToolbar()
-    {
-        ToolbarHelper::title(Text::_('COM_KUNENATOPIC2ARTICLE'), 'article');
-        ToolbarHelper::custom('topic.save', 'save', 'save', 'Save', false);
-        ToolbarHelper::custom('topic.reset', 'cancel', 'cancel', 'Reset Parameters', false);
-        ToolbarHelper::custom('topic.createArticles', 'publish', 'publish', 'Create Articles', false);
     }
 }
