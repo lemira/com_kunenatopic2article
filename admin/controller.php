1b6deadfb05ded2d0cc2be8a79ca819d6951348d
lemira <lemirat@gmail.com>
2025-05-16 21:46:09 +0200

    Update controller.php
    Регистрируем задачи - ДОЛЖНО БЫТЬ ВНУТРИ МЕТОДА __construct

--- admin/controller.php
+++ admin/controller.php
@@ -1,6 +1,5 @@
 <?php
 defined('_JEXEC') or die('Restricted access');
-
 jimport('joomla.application.component.controller');
 
 $logFile = JPATH_BASE . '/logs/controller_debug.log';
@@ -15,14 +14,14 @@ class KunenaTopic2ArticleController extends JControllerLegacy
     public function __construct($config = array())
     {
         parent::__construct($config);
-
+        
+        // Регистрируем задачи - ДОЛЖНО БЫТЬ ВНУТРИ МЕТОДА __construct
+        $this->registerTask('topic.reset', 'reset');
+        
         $logFile = JPATH_BASE . '/logs/controller_debug.log';
         $message = "Constructing KunenaTopic2ArticleController at " . date('Y-m-d H:i:s') . "\n";
         file_put_contents($logFile, $message, FILE_APPEND | FILE_IGNORE_NEW_LINES);
     }
-
-    // Регистрируем задачи
-    $this->registerTask('topic.reset', 'reset');
     
     public function display($cachable = false, $urlparams = false)
     {
@@ -47,47 +46,4 @@ class KunenaTopic2ArticleController extends JControllerLegacy
 
         return $this;
     }
-    
-public function reset()
-{
-    $logFile = JPATH_BASE . '/logs/controller_debug.log';
-    $message = "Reset method called in main controller at " . date('Y-m-d H:i:s') . "\n";
-    file_put_contents($logFile, $message, FILE_APPEND | FILE_IGNORE_NEW_LINES);
-    
-    $app = JFactory::getApplication();
-    $app->enqueueMessage('Reset task triggered in main controller', 'message');
-    
-    try {
-        // Получаем модель
-        $model = $this->getModel('Topic');
-        
-        if (!$model) {
-            $message = "Model 'Topic' not found in reset method at " . date('Y-m-d H:i:s') . "\n";
-            file_put_contents($logFile, $message, FILE_APPEND | FILE_IGNORE_NEW_LINES);
-            throw new Exception('Unable to get Topic model');
-        }
-        
-        $message = "Model class: " . get_class($model) . " at " . date('Y-m-d H:i:s') . "\n";
-        file_put_contents($logFile, $message, FILE_APPEND | FILE_IGNORE_NEW_LINES);
-        
-        if (method_exists($model, 'reset')) {
-            if ($model->reset()) {
-                $app->enqueueMessage('Parameters reset to default values', 'success');
-            } else {
-                $app->enqueueMessage('Failed to reset parameters', 'error');
-            }
-        } else {
-            $message = "Reset method not found in model at " . date('Y-m-d H:i:s') . "\n";
-            file_put_contents($logFile, $message, FILE_APPEND | FILE_IGNORE_NEW_LINES);
-            throw new Exception('Reset method not found in model');
-        }
-    } catch (Exception $e) {
-        $message = "Error in reset: " . $e->getMessage() . " at " . date('Y-m-d H:i:s') . "\n";
-        file_put_contents($logFile, $message, FILE_APPEND | FILE_IGNORE_NEW_LINES);
-        $app->enqueueMessage('Error: ' . $e->getMessage(), 'error');
-    }
-    
-    $this->setRedirect('index.php?option=com_kunenatopic2article&view=topics');
-}
-    
 }
