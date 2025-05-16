e0689e9b3c86eb394887ba815c87b4f8110110f2
lemira <lemirat@gmail.com>
2025-05-16 22:48:39 +0200

    Update default.php
    <button type="button" class="btn btn-secondary mr-2" onclick="Joomla.submitbutton('reset')">
        <?php echo JText::_('COM_KUNENATOPIC2ARTICLE_BUTTON_RESET'); ?>
    </button>

--- admin/views/topics/tmpl/default.php
+++ admin/views/topics/tmpl/default.php
@@ -11,14 +11,12 @@ JHtml::_('formbehavior.chosen', 'select');
         
         <div class="btn-toolbar mb-3">
             <button type="button" class="btn btn-primary mr-2" onclick="Joomla.submitbutton('topic.save')"><?php echo JText::_('COM_KUNENATOPIC2ARTICLE_BUTTON_REMEMBER'); ?></button>
-           <button type="button" class="btn btn-secondary mr-2" onclick="Joomla.submitbutton('topic.reset')">
-    <?php echo JText::_('COM_KUNENATOPIC2ARTICLE_BUTTON_RESET'); ?>
-</button>
+            <button type="button" class="btn btn-secondary mr-2" onclick="Joomla.submitbutton('reset')"><?php echo JText::_('COM_KUNENATOPIC2ARTICLE_BUTTON_RESET'); ?></button>
             <a href="<?php echo JRoute::_('index.php?option=com_kunenatopic2article&task=create'); ?>" class="btn btn-success"><?php echo JText::_('COM_KUNENATOPIC2ARTICLE_BUTTON_CREATE'); ?></a>
         </div>
         
         <h3><?php echo JText::_('COM_KUNENATOPIC2ARTICLE_ARTICLE_PARAMS'); ?></h3>
-        <?php if ($this->form): ?>
+        <?php if ($this->form): ?> 
             <?php echo $this->form->renderFieldset('article_params'); ?>
            <?php else: ?>
             <?php JFactory::getApplication()->enqueueMessage('Form object is empty', 'error'); ?>
