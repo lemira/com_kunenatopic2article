c08087f880fe83a43af10575bfbb9b3282f6f01b
lemira <lemirat@gmail.com>
2025-05-16 22:20:44 +0200

    Update default.php
    reset из основного контроллера

--- admin/views/topics/tmpl/default.php
+++ admin/views/topics/tmpl/default.php
@@ -11,9 +11,9 @@ JHtml::_('formbehavior.chosen', 'select');
         
         <div class="btn-toolbar mb-3">
             <button type="button" class="btn btn-primary mr-2" onclick="Joomla.submitbutton('topic.save')"><?php echo JText::_('COM_KUNENATOPIC2ARTICLE_BUTTON_REMEMBER'); ?></button>
-            <button type="button" class="btn btn-secondary mr-2" onclick="Joomla.submitbutton('kunenatopic2article.reset')">
-                <?php echo JText::_('COM_KUNENATOPIC2ARTICLE_BUTTON_RESET'); ?>
-            </button>
+           <button type="button" class="btn btn-secondary mr-2" onclick="Joomla.submitbutton('topic.reset')">
+    <?php echo JText::_('COM_KUNENATOPIC2ARTICLE_BUTTON_RESET'); ?>
+</button>
             <a href="<?php echo JRoute::_('index.php?option=com_kunenatopic2article&task=create'); ?>" class="btn btn-success"><?php echo JText::_('COM_KUNENATOPIC2ARTICLE_BUTTON_CREATE'); ?></a>
         </div>
         
