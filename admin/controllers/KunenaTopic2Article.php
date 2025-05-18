78b6683a9ca2b7f9f35ac019358575d35650d658
lemira <lemirat@gmail.com>
2025-05-18 06:43:09 +0200

    Update KunenaTopic2Article.php
    перенес метод save() в основной контроллер.

--- admin/controllers/KunenaTopic2Article.php
+++ admin/controllers/KunenaTopic2Article.php
@@ -16,30 +16,4 @@ class KunenaTopic2ArticleController extends JControllerLegacy
         return parent::display($cachable, $urlparams);
     }
 
-    public function save()
-    {
-        $app = JFactory::getApplication();
-        $app->enqueueMessage('Save task triggered', 'message');
-        
-        $task = $app->input->get('task', '', 'string');
-        $app->enqueueMessage('Task received: ' . $task, 'message');
-        
-        $data = $app->input->get('jform', array(), 'array');
-        $app->enqueueMessage('Form data received: ' . print_r($data, true), 'message');
-        
-        $model = $this->getModel('Topic', 'KunenaTopic2ArticleModel');
-        if ($model->save($data)) {
-            $app->enqueueMessage('Parameters saved successfully', 'success');
-        } else {
-            $app->enqueueMessage('Failed to save parameters', 'error');
-        }
-        
-        $this->setRedirect('index.php?option=com_kunenatopic2article&view=topics');
-    }
-
-     public function create()
-    {
-        JFactory::getApplication()->enqueueMessage('Article creation not implemented yet', 'warning');
-        $this->setRedirect('index.php?option=com_kunenatopic2article&view=topics');
-    }
 }
