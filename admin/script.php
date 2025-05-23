1e3f317ad48d47684f0fecb79f2d6ca5ad63cfe8
lemira <lemirat@gmail.com>
2025-05-23 18:40:04 +0200

    Update script.php
    замеа устаревших вызовов и добавление обработки ошибок для метода uninstall().

--- admin/script.php
+++ admin/script.php
@@ -1,23 +1,62 @@
 <?php
 defined('_JEXEC') or die;
 
+use Joomla\CMS\Factory;
+use Joomla\CMS\Language\Text;
+
 class com_kunenatopic2articleInstallerScript
 {
+    /**
+     * Выполняется перед установкой, обновлением или деинсталляцией
+     *
+     * @param string $type Тип действия (install, update, uninstall)
+     * @param object $parent Объект установщика
+     * @return void
+     */
     public function preflight($type, $parent)
     {
         // Можно добавить проверки перед установкой, если нужно
     }
 
+    /**
+     * Выполняется после установки или обновления
+     *
+     * @param string $type Тип действия (install, update)
+     * @param object $parent Объект установщика
+     * @return void
+     */
     public function postflight($type, $parent)
     {
         // Можно добавить действия после установки, если нужно
     }
 
+    /**
+     * Выполняется при деинсталляции компонента
+     *
+     * @param object $parent Объект установщика
+     * @return void
+     */
     public function uninstall($parent)
     {
-        // Удаление таблицы при деинсталляции
-        $db = JFactory::getDbo();
-        $db->setQuery('DROP TABLE IF EXISTS `#__kunenatopic2article_params`');
-        $db->execute();
+        try {
+            // Получаем объект базы данных
+            $db = Factory::getDatabase();
+            
+            // Удаляем таблицу параметров
+            $db->setQuery('DROP TABLE IF EXISTS `#__kunenatopic2article_params`');
+            $db->execute();
+            
+            // Сообщение об успешной деинсталляции
+            Factory::getApplication()->enqueueMessage(
+                Text::_('COM_KUNENATOPIC2ARTICLE_TABLE_PARAMS_DROPPED'),
+                'success'
+            );
+        } catch (\Exception $e) {
+            // Сообщение об ошибке
+            Factory::getApplication()->enqueueMessage(
+                Text::sprintf('COM_KUNENATOPIC2ARTICLE_UNINSTALL_ERROR', $e->getMessage()),
+                'error'
+            );
+        }
     }
 }
