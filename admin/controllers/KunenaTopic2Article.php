ba0b16bdd963a97c15350664ab5c5b1627bd024c
lemira <lemirat@gmail.com>
2025-05-23 13:59:15 +0200

    Update KunenaTopic2Article.php
    замена JControllerLegacy на Joomla\CMS\MVC\Controller\BaseController и JFactory на Joomla\CMS\Factory

--- admin/controllers/KunenaTopic2Article.php
+++ admin/controllers/KunenaTopic2Article.php
@@ -1,19 +1,24 @@
 <?php
 defined('_JEXEC') or die;
 
-class KunenaTopic2ArticleController extends JControllerLegacy
+use Joomla\CMS\Factory;
+use Joomla\CMS\MVC\Controller\BaseController;
+
+/**
+ * KunenaTopic2Article Default Controller
+ */
+class KunenaTopic2ArticleController extends BaseController
 {
     public function __construct($config = array())
     {
         parent::__construct($config);
-        JFactory::getApplication()->enqueueMessage('Controller KunenaTopic2ArticleController initialized', 'message');
+        Factory::getApplication()->enqueueMessage('Controller KunenaTopic2ArticleController initialized', 'message');
     }
 
     public function display($cachable = false, $urlparams = false)
     {
-        JFactory::getApplication()->enqueueMessage('Display called', 'message');
+        Factory::getApplication()->enqueueMessage('Display called', 'message');
         $this->setRedirect('index.php?option=com_kunenatopic2article&view=topics');
         return parent::display($cachable, $urlparams);
     }
-
 }
