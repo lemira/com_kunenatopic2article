4d706e739767fdf8f977704f12787a5d63407ded
lemira <lemirat@gmail.com>
2025-05-23 18:33:19 +0200

    Update kunenatopic2article.php
    загружает KunenaTopic2ArticleControllerArticle из admin/controllers/article.php

--- admin/kunenatopic2article.php
+++ admin/kunenatopic2article.php
@@ -5,13 +5,19 @@ use Joomla\CMS\Factory;
 use Joomla\CMS\Language\Text;
 use Joomla\CMS\MVC\Controller\BaseController;
 
-// Проверка прав доступа (обновлено для Joomla 5)
+// Проверка прав доступа
 $user = Factory::getUser();
 if (!$user->authorise('core.manage', 'com_kunenatopic2article')) {
     throw new \Exception(Text::_('JERROR_ALERTNOAUTHOR'), 403);
 }
 
-// Создание контроллера (обновлено для Joomla 5)
-$controller = BaseController::getInstance('KunenaTopic2Article');
-$controller->execute(Factory::getApplication()->input->get('task'));
+// Отладка для проверки вызова
+$app = Factory::getApplication();
+$app->enqueueMessage('Controller: ' . $app->input->get('controller', 'none'), 'notice');
+$app->enqueueMessage('Task: ' . $app->input->get('task', 'none'), 'notice');
+$app->enqueueMessage('View: ' . $app->input->get('view', 'none'), 'notice');
+
+// Создание контроллера (загружаем KunenaTopic2ArticleControllerArticle)
+$controller = BaseController::getInstance('KunenaTopic2Article', ['controller' => 'Article']);
+$controller->execute($app->input->get('task'));
 $controller->redirect();
