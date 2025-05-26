ad292d3a0050660b0e3df80aa46aea2f14565406
lemira <lemirat@gmail.com>
2025-05-26 19:54:29 +0200

    Update Topic.php
    Испр для Joomla 5

--- admin/models/Topic.php
+++ admin/models/Topic.php
@@ -1,24 +1,43 @@
 <?php
 defined('_JEXEC') or die;
 
-class KunenaTopic2ArticleModelTopic extends JModelAdmin
+use Joomla\CMS\Factory;
+use Joomla\CMS\MVC\Model\AdminModel;
+use Joomla\CMS\Table\Table;
+use Joomla\CMS\Form\Form;
+use Joomla\CMS\Language\Text;
+use Joomla\CMS\Application\CMSApplication;
+use Joomla\Database\DatabaseDriver;
+
+class KunenaTopic2ArticleModelTopic extends AdminModel
 {
-    public function getTable($type = 'Params', $prefix = 'KunenaTopic2ArticleTable', $config = array())
+    protected CMSApplication $app;
+    protected DatabaseDriver $db;
+
+    public function __construct($config = [])
     {
-        $table = JTable::getInstance($type, $prefix, $config);
+        parent::__construct($config);
+        $this->app = Factory::getApplication();
+        $this->db = Factory::getDbo();
+    }
+
+    public function getTable($type = 'Params', $prefix = 'KunenaTopic2ArticleTable', $config = [])
+    {
+        $table = Table::getInstance($type, $prefix, $config);
         if ($table === false) {
-            JFactory::getApplication()->enqueueMessage('Failed to load table ' . $prefix . $type, 'error');
-            return JTable::getInstance('Content');
+            $this->app->enqueueMessage(Text::sprintf('COM_KUNENATOPIC2ARTICLE_TABLE_LOAD_ERROR', $prefix . $type), 'error');
+            return Table::getInstance('Content');
         }
 
         return $table;
     }
 
-    public function getForm($data = array(), $loadData = true)
+    public function getForm($data = [], $loadData = true)
     {
-        $form = $this->loadForm('com_kunenatopic2article.topic', 'topic', array('control' => 'jform', 'load_data' => $loadData));
+        $form = $this->loadForm('com_kunenatopic2article.topic', 'topic', ['control' => 'jform', 'load_data' => $loadData]);
+
         if (empty($form)) {
-            JFactory::getApplication()->enqueueMessage('Failed to load form com_kunenatopic2article.topic', 'error');
+            $this->app->enqueueMessage(Text::_('COM_KUNENATOPIC2ARTICLE_FORM_LOAD_ERROR'), 'error');
             return false;
         }
 
@@ -27,19 +46,17 @@ class KunenaTopic2ArticleModelTopic extends JModelAdmin
 
     protected function loadFormData()
     {
-        $app = JFactory::getApplication();
-        $data = $app->getUserState('com_kunenatopic2article.edit.topic.data', array());
+        $data = $this->app->getUserState('com_kunenatopic2article.edit.topic.data', []);
 
-        // После успешного сохранения или сброса загружаем данные из базы
-        if ($app->getUserState('com_kunenatopic2article.save.success', false)) {
-            $app->setUserState('com_kunenatopic2article.edit.topic.data', array());
-            $app->setUserState('com_kunenatopic2article.save.success', false);
-            $data = array();
+        if ($this->app->getUserState('com_kunenatopic2article.save.success', false)) {
+            $this->app->setUserState('com_kunenatopic2article.edit.topic.data', []);
+            $this->app->setUserState('com_kunenatopic2article.save.success', false);
+            $data = [];
         }
 
         if (empty($data)) {
             $params = $this->getParams();
-            $data = array(
+            $data = [
                 'topic_selection' => (string)$params['topic_selection'],
                 'article_category' => (string)$params['article_category'],
                 'post_transfer_scheme' => (string)$params['post_transfer_scheme'],
@@ -52,7 +69,7 @@ class KunenaTopic2ArticleModelTopic extends JModelAdmin
                 'kunena_post_link' => (string)$params['kunena_post_link'],
                 'reminder_lines' => (string)$params['reminder_lines'],
                 'ignored_authors' => (string)$params['ignored_authors']
-            );
+            ];
         }
 
         return $data;
@@ -60,16 +77,16 @@ class KunenaTopic2ArticleModelTopic extends JModelAdmin
 
     public function getParams()
     {
-        $db = JFactory::getDbo();
-        $query = $db->getQuery(true);
-        $query->select('*')
-              ->from('#__kunenatopic2article_params')
-              ->where('id = 1');
-        $db->setQuery($query);
-        $row = $db->loadAssoc();
+        $query = $this->db->getQuery(true)
+            ->select('*')
+            ->from($this->db->quoteName('#__kunenatopic2article_params'))
+            ->where($this->db->quoteName('id') . ' = 1');
+
+        $this->db->setQuery($query);
+        $row = $this->db->loadAssoc();
 
         if (empty($row)) {
-            return array(
+            return [
                 'topic_selection' => '0',
                 'article_category' => '0',
                 'post_transfer_scheme' => '1',
@@ -82,7 +99,7 @@ class KunenaTopic2ArticleModelTopic extends JModelAdmin
                 'kunena_post_link' => '0',
                 'reminder_lines' => '0',
                 'ignored_authors' => ''
-            );
+            ];
         }
 
         return $row;
@@ -91,76 +108,68 @@ class KunenaTopic2ArticleModelTopic extends JModelAdmin
     public function save($data)
     {
         if (empty($data)) {
-            JFactory::getApplication()->enqueueMessage('No form data to save', 'error');
+            $this->app->enqueueMessage(Text::_('COM_KUNENATOPIC2ARTICLE_NO_DATA_TO_SAVE'), 'error');
             return false;
         }
 
-        $db = JFactory::getDbo();
-        $query = $db->getQuery(true);
-        $query->update('#__kunenatopic2article_params')
-              ->set('topic_selection = ' . $db->quote($data['topic_selection']))
-              ->set('article_category = ' . $db->quote($data['article_category']))
-              ->set('post_transfer_scheme = ' . $db->quote($data['post_transfer_scheme']))
-              ->set('max_article_size = ' . $db->quote($data['max_article_size']))
-              ->set('post_author = ' . $db->quote($data['post_author']))
-              ->set('post_creation_date = ' . $db->quote($data['post_creation_date']))
-              ->set('post_creation_time = ' . $db->quote($data['post_creation_time']))
-              ->set('post_ids = ' . $db->quote($data['post_ids']))
-              ->set('post_title = ' . $db->quote($data['post_title']))
-              ->set('kunena_post_link = ' . $db->quote($data['kunena_post_link']))
-              ->set('reminder_lines = ' . $db->quote($data['reminder_lines']))
-              ->set('ignored_authors = ' . $db->quote($data['ignored_authors']))
-              ->where('id = 1');
-        $db->setQuery($query);
+        $query = $this->db->getQuery(true)
+            ->update($this->db->quoteName('#__kunenatopic2article_params'))
+            ->set([
+                $this->db->quoteName('topic_selection') . ' = ' . $this->db->quote($data['topic_selection']),
+                $this->db->quoteName('article_category') . ' = ' . $this->db->quote($data['article_category']),
+                $this->db->quoteName('post_transfer_scheme') . ' = ' . $this->db->quote($data['post_transfer_scheme']),
+                $this->db->quoteName('max_article_size') . ' = ' . $this->db->quote($data['max_article_size']),
+                $this->db->quoteName('post_author') . ' = ' . $this->db->quote($data['post_author']),
+                $this->db->quoteName('post_creation_date') . ' = ' . $this->db->quote($data['post_creation_date']),
+                $this->db->quoteName('post_creation_time') . ' = ' . $this->db->quote($data['post_creation_time']),
+                $this->db->quoteName('post_ids') . ' = ' . $this->db->quote($data['post_ids']),
+                $this->db->quoteName('post_title') . ' = ' . $this->db->quote($data['post_title']),
+                $this->db->quoteName('kunena_post_link') . ' = ' . $this->db->quote($data['kunena_post_link']),
+                $this->db->quoteName('reminder_lines') . ' = ' . $this->db->quote($data['reminder_lines']),
+                $this->db->quoteName('ignored_authors') . ' = ' . $this->db->quote($data['ignored_authors'])
+            ])
+            ->where($this->db->quoteName('id') . ' = 1');
+
+        $this->db->setQuery($query);
 
         try {
-            $db->execute();
-            $app = JFactory::getApplication();
-            $app->setUserState('com_kunenatopic2article.save.success', true);
+            $this->db->execute();
+            $this->app->setUserState('com_kunenatopic2article.save.success', true);
             return true;
         } catch (Exception $e) {
-            JFactory::getApplication()->enqueueMessage('Save failed with error: ' . $e->getMessage(), 'error');
+            $this->app->enqueueMessage(Text::_('COM_KUNENATOPIC2ARTICLE_SAVE_FAILED') . ': ' . $e->getMessage(), 'error');
             return false;
         }
     }
-public function reset()
-{
-    $app = JFactory::getApplication();
-    try {
-        // Подключение к базе данных
-        $db = JFactory::getDbo();
-        
-        // Значения по умолчанию из install.mysql.utf8.sql
-        $query = $db->getQuery(true)
-            ->update($db->quoteName('#__kunenatopic2article_params'))
-            ->set([
-                $db->quoteName('topic_selection') . ' = 0',
-                $db->quoteName('article_category') . ' = 0',
-                $db->quoteName('post_transfer_scheme') . ' = 1',
-                $db->quoteName('max_article_size') . ' = 40000',
-                $db->quoteName('post_author') . ' = 1',
-                $db->quoteName('post_creation_date') . ' = 0',
-                $db->quoteName('post_creation_time') . ' = 0',
-                $db->quoteName('post_ids') . ' = 0',
-                $db->quoteName('post_title') . ' = 0',
-                $db->quoteName('kunena_post_link') . ' = 0',
-                $db->quoteName('reminder_lines') . ' = 0',
-                $db->quoteName('ignored_authors') . ' = ' . $db->quote('')
-            ])
-            ->where($db->quoteName('id') . ' = 1');
-        
-        $db->setQuery($query);
-        $result = $db->execute();
-        
-        if (!$result) {
-            throw new Exception($db->getErrorMsg());
+
+    public function reset()
+    {
+        try {
+            $query = $this->db->getQuery(true)
+                ->update($this->db->quoteName('#__kunenatopic2article_params'))
+                ->set([
+                    $this->db->quoteName('topic_selection') . ' = 0',
+                    $this->db->quoteName('article_category') . ' = 0',
+                    $this->db->quoteName('post_transfer_scheme') . ' = 1',
+                    $this->db->quoteName('max_article_size') . ' = 40000',
+                    $this->db->quoteName('post_author') . ' = 1',
+                    $this->db->quoteName('post_creation_date') . ' = 0',
+                    $this->db->quoteName('post_creation_time') . ' = 0',
+                    $this->db->quoteName('post_ids') . ' = 0',
+                    $this->db->quoteName('post_title') . ' = 0',
+                    $this->db->quoteName('kunena_post_link') . ' = 0',
+                    $this->db->quoteName('reminder_lines') . ' = 0',
+                    $this->db->quoteName('ignored_authors') . ' = ' . $this->db->quote('')
+                ])
+                ->where($this->db->quoteName('id') . ' = 1');
+
+            $this->db->setQuery($query);
+            $this->db->execute();
+
+            return true;
+        } catch (Exception $e) {
+            $this->app->enqueueMessage(Text::_('COM_KUNENATOPIC2ARTICLE_RESET_FAILED') . ': ' . $e->getMessage(), 'error');
+            return false;
         }
-        
-        return true;
-    } catch (Exception $e) {
-        $app->enqueueMessage('Error in reset method: ' . $e->getMessage(), 'error');
-        return false;
     }
 }
-    
- }
