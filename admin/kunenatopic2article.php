373706b29112e46de0e95ba95645356837eaeacf
lemira <lemirat@gmail.com>
2025-05-26 18:25:51 +0200

    Update kunenatopic2article.php
    Убраны // Если задача не указана, устанавливаем задачу по умолчанию
    if (empty($task)) {
        $task = 'display';
    } 
    и $view = $input->get('view', '');

--- admin/kunenatopic2article.php
+++ admin/kunenatopic2article.php
@@ -1,56 +1,38 @@
 <?php
 /**
- * @package     com_kunenatopic2article
+ * @package     Joomla.Administrator
  * @subpackage  com_kunenatopic2article
- * @author      lemira
- * @license     GNU General Public License version 2 or later
+ *
+ * @copyright   Copyright (C) 2023 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 
-defined('_JEXEC') or die;
+// No direct access to this file
+defined('_JEXEC') or die('Restricted access');
 
 use Joomla\CMS\Factory;
-use Joomla\CMS\Language\Text;
 use Joomla\CMS\MVC\Controller\BaseController;
+use Joomla\CMS\Language\Text;
 
-// Проверка прав доступа
-$app = Factory::getApplication();
-$user = $app->getIdentity();
-
-if (!$user->authorise('core.manage', 'com_kunenatopic2article')) {
+// Access check.
+if (!Factory::getUser()->authorise('core.manage', 'com_kunenatopic2article')) {
     throw new \Exception(Text::_('JERROR_ALERTNOAUTHOR'), 403);
 }
 
-// Отладка
 $app = Factory::getApplication();
 $input = $app->input;
-$app->enqueueMessage('Request URL: ' . $app->get('uri.request'), 'notice');
-$app->enqueueMessage('Controller: ' . $input->get('controller', 'none'), 'notice');
-$app->enqueueMessage('Task: ' . $input->get('task', 'none'), 'notice');
-$app->enqueueMessage('View: ' . $input->get('view', 'none'), 'notice');
-$app->enqueueMessage('Format: ' . $input->get('format', 'none'), 'notice');
 
-// Создание контроллера
-$controller = BaseController::getInstance('KunenaTopic2Article', ['controller' => 'Article']);
+// Get an instance of the controller prefixed by KunenaTopic2Article
+$controller = BaseController::getInstance('KunenaTopic2Article');
 
-// Проверяем задачу и представление
-$task = $input->get('task', '');
-$view = $input->get('view', '');
+// Perform the Request task
+$task = $input->getCmd('task');
 
-// Если указан view=topics без задачи, вызываем display()
-if (empty($task) && $view === 'topics') {
-    $app->enqueueMessage('Loading topics view via display()', 'notice');
-    $controller->display();
-    return;
+try {
+    $controller->execute($task);
+} catch (Exception $e) {
+    $app->enqueueMessage($e->getMessage(), 'error');
 }
 
-// Если нет задачи и представления, перенаправляем на view=topics
-if (empty($task) && empty($view)) {
-    $app->enqueueMessage('No task or view provided, redirecting to default view', 'notice');
-    $controller->setRedirect('index.php?option=com_kunenatopic2article&view=topics');
-    $controller->redirect();
-    return;
-}
-
-// Выполняем задачу
-$controller->execute($task);
+// Redirect if set by the controller
 $controller->redirect();
