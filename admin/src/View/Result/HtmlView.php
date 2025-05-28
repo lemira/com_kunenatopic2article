1c6b81d8d169f87d7c530db2cac66208d1d6789d
lemira <lemirat@gmail.com>
2025-05-28 17:39:49 +0200

    Create HtmlView.php
    новый вью

---
+++ admin/src/View/Result/HtmlView.php
@@ -0,0 +1,19 @@
+// Файл: admin/src/View/Result/HtmlView.php
+namespace Joomla\Component\KunenaTopic2Article\Administrator\View\Result;
+
+\defined('_JEXEC') or die;
+
+use Joomla\CMS\MVC\View\HtmlView as BaseHtmlView;
+use Joomla\CMS\Factory;
+
+class HtmlView extends BaseHtmlView
+{
+    protected $links;
+
+    public function display($tpl = null)
+    {
+        $app = Factory::getApplication();
+        $this->links = $app->getUserState('com_kunenatopic2article.article_links', []);
+        parent::display($tpl);
+    }
+}
