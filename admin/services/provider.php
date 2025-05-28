474fa7392e68850b6630961f9d021bb322579dfa
lemira <lemirat@gmail.com>
2025-05-28 19:57:18 +0200

    Update provider.php

--- admin/services/provider.php
+++ admin/services/provider.php
@@ -1,14 +1,14 @@
 <?php
-// Файл: admin/services/provider.php
-
 \defined('_JEXEC') or die;
 
+use Joomla\CMS\Dispatcher\ComponentDispatcherFactoryInterface;
 use Joomla\CMS\Extension\ComponentInterface;
 use Joomla\CMS\Extension\Service\Provider\ComponentDispatcherFactory;
 use Joomla\CMS\Extension\Service\Provider\MVCFactory;
 use Joomla\CMS\MVC\Factory\MVCFactoryInterface;
 use Joomla\DI\Container;
 use Joomla\DI\ServiceProviderInterface;
+use Joomla\Component\KunenaTopic2Article\Administrator\Extension\KunenaTopic2ArticleComponent;
 
 return new class implements ServiceProviderInterface
 {
@@ -20,11 +20,10 @@ return new class implements ServiceProviderInterface
         $container->set(
             ComponentInterface::class,
             function (Container $container) {
-                $component = new \Joomla\CMS\Extension\Component(
-                    $container->get(\Joomla\CMS\Dispatcher\ComponentDispatcherFactoryInterface::class)
+                $component = new KunenaTopic2ArticleComponent(
+                    $container->get(ComponentDispatcherFactoryInterface::class)
                 );
                 $component->setMVCFactory($container->get(MVCFactoryInterface::class));
-
                 return $component;
             }
         );
