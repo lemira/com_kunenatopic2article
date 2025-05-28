c6b0083b31aefd1cb62f1c56ec12cbcc7a58f8b4
lemira <lemirat@gmail.com>
2025-05-28 19:56:06 +0200

    Create KunenaTopic2ArticleComponent.php
    класс компонента

---
+++ admin/src/Extension/KunenaTopic2ArticleComponent.php
@@ -0,0 +1,27 @@
+<?php
+namespace Joomla\Component\KunenaTopic2Article\Administrator\Extension;
+
+\defined('_JEXEC') or die;
+
+use Joomla\CMS\Extension\BootableExtensionInterface;
+use Joomla\CMS\Extension\MVCComponent;
+use Joomla\CMS\HTML\HTMLRegistryAwareTrait;
+use Psr\Container\ContainerInterface;
+
+class KunenaTopic2ArticleComponent extends MVCComponent implements BootableExtensionInterface
+{
+    use HTMLRegistryAwareTrait;
+
+    /**
+     * Booting the extension. This is the function to set up the environment of the extension like
+     * registering new class loaders, etc.
+     *
+     * @param   ContainerInterface  $container  The container
+     *
+     * @return  void
+     */
+    public function boot(ContainerInterface $container)
+    {
+        // Здесь можно добавить инициализацию если нужно
+    }
+}
