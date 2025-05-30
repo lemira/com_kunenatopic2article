868105eb1c0522bf2e615aff81d948162fbdc426
lemira <lemirat@gmail.com>
2025-05-30 22:38:42 +0200

    Create provider.php
    тестовый упрощенный вариант
    30.05 22;0 кл

---
+++ admin/services/provider.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * @package     KunenaTopic2Article
+ * @subpackage  com_kunenatopic2article
+ */
+
+defined('_JEXEC') or die;
+
+use Joomla\CMS\Dispatcher\ComponentDispatcherFactoryInterface;
+use Joomla\CMS\Extension\ComponentInterface;
+use Joomla\CMS\Extension\Service\Provider\ComponentDispatcherFactory;
+use Joomla\CMS\Extension\Service\Provider\MVCFactory;
+use Joomla\CMS\HTML\Registry;
+use Joomla\CMS\MVC\Factory\MVCFactoryInterface;
+use Joomla\Component\KunenaTopic2Article\Administrator\Extension\KunenaTopic2ArticleComponent;
+use Joomla\DI\Container;
+use Joomla\DI\ServiceProviderInterface;
+
+/**
+ * Service provider for KunenaTopic2Article component
+ */
+return new class implements ServiceProviderInterface
+{
+    public function register(Container $container)
+    {
+        // Регистрируем MVC фабрику
+        $container->registerServiceProvider(new MVCFactory('\\Joomla\\Component\\KunenaTopic2Article\\Administrator'));
+        
+        // Регистрируем диспетчер
+        $container->registerServiceProvider(new ComponentDispatcherFactory('\\Joomla\\Component\\KunenaTopic2Article\\Administrator'));
+
+        // Регистрируем главный компонент
+        $container->set(
+            ComponentInterface::class,
+            function (Container $container) {
+                $component = new KunenaTopic2ArticleComponent($container->get(ComponentDispatcherFactoryInterface::class));
+                $component->setRegistry($container->get(Registry::class));
+                $component->setMVCFactory($container->get(MVCFactoryInterface::class));
+                return $component;
+            }
+        );
+    }
+};
