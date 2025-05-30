83f7daea39212a987a05f4ebfee314459642f3c5
lemira <lemirat@gmail.com>
2025-05-30 18:50:53 +0200

    Update KunenaTopic2ArticleComponent.php
    упростил 
    Убрал getContainerExtension() - регистрация происходит в services/provider.php

--- admin/src/Extension/KunenaTopic2ArticleComponent.php
+++ admin/src/Extension/KunenaTopic2ArticleComponent.php
@@ -1,3 +1,4 @@
+<?php
 namespace Joomla\Component\KunenaTopic2Article\Administrator\Extension;
 
 \defined('_JEXEC') or die;
@@ -5,34 +6,14 @@ namespace Joomla\Component\KunenaTopic2Article\Administrator\Extension;
 use Joomla\CMS\Extension\BootableExtensionInterface;
 use Joomla\CMS\Extension\MVCComponent;
 use Joomla\CMS\HTML\HTMLRegistryAwareTrait;
-use Joomla\Component\KunenaTopic2Article\Administrator\Service\Provider\KunenaTopic2ArticleServiceProvider;
-use Joomla\CMS\DependencyInjection\ServiceRegistryInterface;
-use Joomla\DI\Container;
 use Psr\Container\ContainerInterface;
 
 class KunenaTopic2ArticleComponent extends MVCComponent implements BootableExtensionInterface
 {
     use HTMLRegistryAwareTrait;
-
+    
     public function boot(ContainerInterface $container): void
     {
-        // При необходимости – инициализация
-    }
-
-    public function getContainerExtension(): ?ServiceRegistryInterface
-    {
-        return new class (new KunenaTopic2ArticleServiceProvider()) implements ServiceRegistryInterface {
-            private KunenaTopic2ArticleServiceProvider $provider;
-
-            public function __construct(KunenaTopic2ArticleServiceProvider $provider)
-            {
-                $this->provider = $provider;
-            }
-
-            public function register(Container $container): void
-            {
-                $this->provider->register($container);
-            }
-        };
+        // Инициализация при необходимости
     }
 }
