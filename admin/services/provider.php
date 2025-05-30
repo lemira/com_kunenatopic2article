4ea4681103fd195fc45fcd9bc198ec1aff80c845
lemira <lemirat@gmail.com>
2025-05-30 19:49:29 +0200

    Update provider.php
    для правильной регистрации компонента
    30.05 19:50 кл

--- admin/services/provider.php
+++ admin/services/provider.php
@@ -1,15 +1,41 @@
 <?php
-\defined('_JEXEC') or die;
+/**
+ * @package     KunenaTopic2Article
+ * @subpackage  com_kunenatopic2article
+ * @author      Your Name
+ * @copyright   Copyright (C) 2024 Your Name. All rights reserved.
+ * @license     GNU General Public License version 2 or later
+ */
 
+defined('_JEXEC') or die;
+
+use Joomla\CMS\Dispatcher\ComponentDispatcherFactoryInterface;
 use Joomla\CMS\Extension\ComponentInterface;
 use Joomla\CMS\Extension\Service\Provider\ComponentDispatcherFactory;
 use Joomla\CMS\Extension\Service\Provider\MVCFactory;
+use Joomla\CMS\HTML\Registry;
+use Joomla\CMS\MVC\Factory\MVCFactoryInterface;
 use Joomla\Component\KunenaTopic2Article\Administrator\Extension\KunenaTopic2ArticleComponent;
 use Joomla\DI\Container;
 use Joomla\DI\ServiceProviderInterface;
 
-return new class () implements ServiceProviderInterface {
-    public function register(Container $container): void
+/**
+ * The KunenaTopic2Article service provider.
+ *
+ * @since  1.0.0
+ */
+return new class implements ServiceProviderInterface
+{
+    /**
+     * Registers the service provider with a DI container.
+     *
+     * @param   Container  $container  The DI container.
+     *
+     * @return  void
+     *
+     * @since   1.0.0
+     */
+    public function register(Container $container)
     {
         $container->registerServiceProvider(new MVCFactory('\\Joomla\\Component\\KunenaTopic2Article'));
         $container->registerServiceProvider(new ComponentDispatcherFactory('\\Joomla\\Component\\KunenaTopic2Article'));
@@ -17,9 +43,12 @@ return new class () implements ServiceProviderInterface {
         $container->set(
             ComponentInterface::class,
             function (Container $container) {
-                return new KunenaTopic2ArticleComponent(
-                    $container->get(\Joomla\CMS\Dispatcher\ComponentDispatcherFactoryInterface::class)
-                );
+                $component = new KunenaTopic2ArticleComponent($container->get(ComponentDispatcherFactoryInterface::class));
+
+                $component->setRegistry($container->get(Registry::class));
+                $component->setMVCFactory($container->get(MVCFactoryInterface::class));
+
+                return $component;
             }
         );
     }
