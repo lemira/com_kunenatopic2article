51675009a83ceaefae277205f301a8c4b9c78b51
lemira <lemirat@gmail.com>
2025-05-30 18:27:39 +0200

    Create provider.php
    Исправленный
    30.05 кл

---
+++ admin/services/provider.php
@@ -0,0 +1,33 @@
+<?php
+\defined('_JEXEC') or die;
+
+use Joomla\CMS\Extension\ComponentInterface;
+use Joomla\CMS\Extension\Service\Provider\ComponentDispatcherFactory;
+use Joomla\CMS\Extension\Service\Provider\MVCFactory;
+use Joomla\CMS\HTML\Registry;
+use Joomla\CMS\MVC\Factory\MVCFactoryInterface;
+use Joomla\Component\KunenaTopic2Article\Administrator\Extension\KunenaTopic2ArticleComponent;
+use Joomla\DI\Container;
+use Joomla\DI\ServiceProviderInterface;
+
+return new class () implements ServiceProviderInterface {
+    public function register(Container $container): void
+    {
+        $container->registerServiceProvider(new MVCFactory('\\Joomla\\Component\\KunenaTopic2Article'));
+        $container->registerServiceProvider(new ComponentDispatcherFactory('\\Joomla\\Component\\KunenaTopic2Article'));
+
+        $container->set(
+            ComponentInterface::class,
+            function (Container $container) {
+                $component = new KunenaTopic2ArticleComponent(
+                    $container->get(ComponentDispatcherFactoryInterface::class)
+                );
+
+                $component->setRegistry($container->get(Registry::class));
+                $component->setMVCFactory($container->get(MVCFactoryInterface::class));
+
+                return $component;
+            }
+        );
+    }
+};
