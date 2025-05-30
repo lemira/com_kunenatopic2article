81a39112bdac723d0ff36a8500411445e31f79a1
lemira <lemirat@gmail.com>
2025-05-30 14:40:38 +0200

    Update KunenaTopic2ArticleComponent.php
    Добавил в  класс метод getContainerExtension(), который вернёт анонимную реализацию интерфейса ServiceRegistryInterface и внутри зарегистрирует ваш KunenaTopic2ArticleServiceProvider.

--- admin/src/Extension/KunenaTopic2ArticleComponent.php
+++ admin/src/Extension/KunenaTopic2ArticleComponent.php
@@ -6,22 +6,39 @@ namespace Joomla\Component\KunenaTopic2Article\Administrator\Extension;
 use Joomla\CMS\Extension\BootableExtensionInterface;
 use Joomla\CMS\Extension\MVCComponent;
 use Joomla\CMS\HTML\HTMLRegistryAwareTrait;
+use Joomla\Component\KunenaTopic2Article\Administrator\Service\Provider\KunenaTopic2ArticleServiceProvider;
+use Joomla\CMS\DependencyInjection\ServiceRegistryInterface;
 use Psr\Container\ContainerInterface;
+use Joomla\DI\Container;
 
 class KunenaTopic2ArticleComponent extends MVCComponent implements BootableExtensionInterface
 {
     use HTMLRegistryAwareTrait;
 
     /**
-     * Booting the extension. This is the function to set up the environment of the extension like
-     * registering new class loaders, etc.
+     * Boot the component.
      *
-     * @param   ContainerInterface  $container  The container
+     * @param   ContainerInterface  $container  The DI container
      *
-     * @return  void
+     * @return void
      */
-    public function boot(ContainerInterface $container)
+    public function boot(ContainerInterface $container): void
     {
-        // Здесь можно добавить инициализацию если нужно
+        // Здесь можно добавить дополнительную инициализацию, если необходимо
+    }
+
+    /**
+     * Registers the component's service provider.
+     *
+     * @return ServiceRegistryInterface|null
+     */
+    public function getContainerExtension(): ?ServiceRegistryInterface
+    {
+        return new class implements ServiceRegistryInterface {
+            public function register(Container $container): void
+            {
+                (new KunenaTopic2ArticleServiceProvider())->register($container);
+            }
+        };
     }
 }
