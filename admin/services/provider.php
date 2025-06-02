d2989f2bd8b646c131d22599b24b588c80d84740
lemira <lemirat@gmail.com>
2025-06-02 21:27:14 +0200

    Update provider.php
    2.6 21:28 дс

--- admin/services/provider.php
+++ admin/services/provider.php
@@ -13,16 +13,18 @@ return new class implements ServiceProviderInterface
 {
     public function register(Container $container)
     {
-        // Регистрация namespace PSR-4
+        // Явная регистрация пути (важно для admin/ структуры)
         $container->set(
             ComponentInterface::class,
             function (Container $container) {
+                require_once JPATH_ADMINISTRATOR.'/components/com_kunenatopic2article/admin/src/Extension/KunenaTopic2ArticleComponent.php';
+                
                 $component = new KunenaTopic2ArticleComponent(
                     $container->get(ComponentDispatcherFactoryInterface::class)
                 );
-
+                
                 $component->setMVCFactory($container->get(MVCFactoryInterface::class));
-
+                
                 return $component;
             }
         );
