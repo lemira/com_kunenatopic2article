8077877a21f1132d01d17f50f230795042bc30f7
lemira <lemirat@gmail.com>
2025-06-02 17:04:11 +0200

    Update provider.php
    файл был избыточен
    2.6.17:05 дс

--- admin/services/provider.php
+++ admin/services/provider.php
@@ -1,61 +1,30 @@
-<?php
 defined('_JEXEC') or die;
 
-// Отладка коп
-//var_dump(is_dir(JPATH_ADMINISTRATOR . '/components/com_kunenatopic2article/src/Controller'));
-// exit;
-// var_dump(file_exists(JPATH_ADMINISTRATOR . '/components/com_kunenatopic2article/src/Controller/DisplayController.php'));
-// exit;
-// var_dump(class_exists('Joomla\Component\KunenaTopic2Article\Administrator\Controller\DisplayController'));
-// exit;
-
-// Принудительная регистрация пространства имен
-JLoader::registerNamespace(
-    'Joomla\\Component\\KunenaTopic2Article\\Administrator',
-    JPATH_ADMINISTRATOR . '/components/com_kunenatopic2article/src',
-    false,
-    false,
-    'psr4'
-);
-
-JLoader::registerNamespace(
-    'Joomla\\Component\\KunenaTopic2Article\\Administrator\\Controller',
-    JPATH_ADMINISTRATOR . '/components/com_kunenatopic2article/src/Controller',
-    false,
-    false,
-    'psr4'
-);
-
-use Joomla\CMS\Dispatcher\ComponentDispatcherFactoryInterface;
 use Joomla\CMS\Extension\ComponentInterface;
 use Joomla\CMS\Extension\Service\Provider\ComponentDispatcherFactory;
 use Joomla\CMS\Extension\Service\Provider\MVCFactory;
-use Joomla\CMS\HTML\Registry;
+use Joomla\CMS\Dispatcher\ComponentDispatcherFactoryInterface;
 use Joomla\CMS\MVC\Factory\MVCFactoryInterface;
 use Joomla\Component\KunenaTopic2Article\Administrator\Extension\KunenaTopic2ArticleComponent;
 use Joomla\DI\Container;
 use Joomla\DI\ServiceProviderInterface;
 
-/**
- * Service provider for KunenaTopic2Article component
- */
 return new class implements ServiceProviderInterface
 {
     public function register(Container $container)
     {
-        // Регистрируем MVC фабрику
-        $container->registerServiceProvider(new MVCFactory('\\Joomla\\Component\\KunenaTopic2Article\\Administrator'));
+        $container->registerServiceProvider(new ComponentDispatcherFactory('\\Joomla\\Component\\KunenaTopic2Article'));
+        $container->registerServiceProvider(new MVCFactory('\\Joomla\\Component\\KunenaTopic2Article'));
         
-        // Регистрируем диспетчер
-        $container->registerServiceProvider(new ComponentDispatcherFactory('\\Joomla\\Component\\KunenaTopic2Article\\Administrator'));
-
-        // Регистрируем главный компонент
         $container->set(
             ComponentInterface::class,
             function (Container $container) {
-                $component = new KunenaTopic2ArticleComponent($container->get(ComponentDispatcherFactoryInterface::class));
-                $component->setRegistry($container->get(Registry::class));
+                $component = new KunenaTopic2ArticleComponent(
+                    $container->get(ComponentDispatcherFactoryInterface::class)
+                );
+                
                 $component->setMVCFactory($container->get(MVCFactoryInterface::class));
+                
                 return $component;
             }
         );
