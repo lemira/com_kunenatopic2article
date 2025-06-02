2b607f8d8672f5b5584b88cfa2d139a0cad85e31
lemira <lemirat@gmail.com>
2025-06-02 22:13:56 +0200

    Update autoload.php
    упрощаем

--- admin/autoload.php
+++ admin/autoload.php
@@ -1,10 +1,5 @@
 defined('_JEXEC') or die;
 
-use Joomla\CMS\Factory;
-
-// Отладка
-Factory::getApplication()->enqueueMessage('Autoload executed', 'notice');
-
 JLoader::registerNamespace(
     'Joomla\\Component\\KunenaTopic2Article',
     JPATH_ADMINISTRATOR . '/components/com_kunenatopic2article/admin/src',
