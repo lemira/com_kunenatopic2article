252fb1461e0e8a03d291f5760ecff0caf7cc6351
lemira <lemirat@gmail.com>
2025-06-03 15:09:13 +0200

    Delete admin/autoload.php
    не нужен
    4а

--- admin/autoload.php
+++
@@ -1,9 +0,0 @@
-defined('_JEXEC') or die;
-
-JLoader::registerNamespace(
-    'Joomla\\Component\\KunenaTopic2Article',
-    JPATH_ADMINISTRATOR . '/components/com_kunenatopic2article/admin/src',
-    false,
-    false,
-    'psr4'
-);
