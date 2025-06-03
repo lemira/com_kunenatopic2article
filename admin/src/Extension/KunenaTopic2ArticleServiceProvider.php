a27c190b49e34d68bd22a108910bc1692d75272d
lemira <lemirat@gmail.com>
2025-06-03 15:48:05 +0200

    Create KunenaTopic2ArticleServiceProvider.php
    вместо admin/services/provider.php
    3.6 4а

---
+++ admin/src/Extension/KunenaTopic2ArticleServiceProvider.php
@@ -0,0 +1,34 @@
+<?php
+namespace Joomla\Component\KunenaTopic2Article\Administrator\Extension;
+
+use Joomla\CMS\Extension\ComponentInterface;
+use Joomla\CMS\Extension\Service\Provider\ComponentDispatcherFactory;
+use Joomla\CMS\Extension\Service\Provider\MVCFactory;
+use Joomla\CMS\Dispatcher\ComponentDispatcherFactoryInterface;
+use Joomla\CMS\MVC\Factory\MVCFactoryInterface;
+use Joomla\DI\Container;
+use Joomla\DI\ServiceProviderInterface;
+
+class KunenaTopic2ArticleServiceProvider implements ServiceProviderInterface
+{
+    public function register(Container $container): void
+    {
+        // Подключаем фабрики MVC и Dispatcher
+        $container->registerServiceProvider(new ComponentDispatcherFactory(__NAMESPACE__));
+        $container->registerServiceProvider(new MVCFactory(__NAMESPACE__));
+
+        // Регистрируем сам компонент
+        $container->set(
+            ComponentInterface::class,
+            function (Container $container) {
+                $component = new KunenaTopic2ArticleComponent(
+                    $container->get(ComponentDispatcherFactoryInterface::class)
+                );
+
+                $component->setMVCFactory($container->get(MVCFactoryInterface::class));
+
+                return $component;
+            }
+        );
+    }
+}
