473e6c19098a17c470d71a1a6b1f81c18b8342cb
lemira <lemirat@gmail.com>
2025-06-04 23:35:19 +0200

    Delete services/provider.php
    перенес в admin/services/provider.php

--- services/provider.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Это точка входа для Service Provider в Joomla 5
-defined('_JEXEC') || exit;
-
-use Joomla\Component\KunenaTopic2Article\Administrator\Extension\KunenaTopic2ArticleServiceProvider;
-
-return new KunenaTopic2ArticleServiceProvider;
