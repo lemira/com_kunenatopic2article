02f0639bd641ea4de771e0b75ccf1fbb3622e2fd
lemira <lemirat@gmail.com>
2025-06-04 23:12:46 +0200

    Create provider.php
    форум https://forum.joomla.org/viewtopic.php?f=834&t=1017607

---
+++ services/provider.php
@@ -0,0 +1,7 @@
+<?php
+// Это точка входа для Service Provider в Joomla 5
+defined('_JEXEC') || exit;
+
+use Joomla\Component\KunenaTopic2Article\Administrator\Extension\KunenaTopic2ArticleServiceProvider;
+
+return new KunenaTopic2ArticleServiceProvider;
