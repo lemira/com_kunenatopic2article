5692e41c7bd54cef1db263085182612c92b6d236
lemira <lemirat@gmail.com>
2025-06-09 23:25:09 +0200

    Update provider.php
    use Joomla\Component\KunenaTopic2Article\Extension\KunenaTopic2ArticleServiceProvider;
    дс

--- admin/services/provider.php
+++ admin/services/provider.php
@@ -2,6 +2,6 @@
 // Это точка входа для Service Provider в Joomla 5
 defined('_JEXEC') || exit;
 
-use Joomla\Component\KunenaTopic2Article\Administrator\Extension\KunenaTopic2ArticleServiceProvider;
+use Joomla\Component\KunenaTopic2Article\Extension\KunenaTopic2ArticleServiceProvider;
 
 return new KunenaTopic2ArticleServiceProvider;
