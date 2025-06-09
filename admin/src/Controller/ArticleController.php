94dfff72e57b13ec5d5cb5d6b6dcf754fd4e5253
lemira <lemirat@gmail.com>
2025-06-09 22:46:31 +0200

    Update ArticleController.php
    Стало:
    namespace Joomla\Component\KunenaTopic2Article\Controller;
    дс

--- admin/src/Controller/ArticleController.php
+++ admin/src/Controller/ArticleController.php
@@ -7,7 +7,7 @@
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 
-namespace Joomla\Component\Kunenatopic2article\Extension\Controller;
+namespace Joomla\Component\Kunenatopic2article\Controller;
 
 // No direct access to this file
 defined('_JEXEC') or die('Restricted access');
