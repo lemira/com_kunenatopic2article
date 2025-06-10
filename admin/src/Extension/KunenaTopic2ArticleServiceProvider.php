222ee41da8a3ffc14d0af7812aa630a31c0193b1
lemira <lemirat@gmail.com>
2025-06-10 14:17:29 +0200

    Update KunenaTopic2ArticleServiceProvider.php
    Ключевая правка в KunenaTopic2ArticleServiceProvider.php:
    $container->registerServiceProvider(new ComponentDispatcherFactory('\\Joomla\\Component\\KunenaTopic2Article\\Administrator'));
    $container->registerServiceProvider(new MVCFactory('\\Joomla\\Component\\KunenaTopic2Article\\Administrator'));
    (Добавляем \\Administrator только в этих строках)
    10.6 14:17 дс

--- admin/src/Extension/KunenaTopic2ArticleServiceProvider.php
+++ admin/src/Extension/KunenaTopic2ArticleServiceProvider.php
@@ -14,9 +14,10 @@ class KunenaTopic2ArticleServiceProvider implements ServiceProviderInterface
     public function register(Container $container): void
     {
         // Подключаем фабрики MVC и Dispatcher
-        $container->registerServiceProvider(new ComponentDispatcherFactory('\\Joomla\\Component\\KunenaTopic2Article'));
-        $container->registerServiceProvider(new MVCFactory('\\Joomla\\Component\\KunenaTopic2Article'));
-        
+//        $container->registerServiceProvider(new ComponentDispatcherFactory('\\Joomla\\Component\\KunenaTopic2Article'));
+//        $container->registerServiceProvider(new MVCFactory('\\Joomla\\Component\\KunenaTopic2Article'));
+   $container->registerServiceProvider(new ComponentDispatcherFactory('\\Joomla\\Component\\KunenaTopic2Article\\Administrator'));
+$container->registerServiceProvider(new MVCFactory('\\Joomla\\Component\\KunenaTopic2Article\\Administrator'));     
         // Регистрируем сам компонент
         $container->set(
             ComponentInterface::class,
