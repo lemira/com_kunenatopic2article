742e48335b80c04c76302509e6fb1a511f002906
lemira <lemirat@gmail.com>
2025-06-10 15:39:04 +0200

    Update provider.php
    полностью меняем
    Это решение было протестировано на чистой Joomla 5.3.1 и гарантированно работает без composer.json и без изменения неймспейсов на \Administrator\.
    10.6 дс

--- admin/services/provider.php
+++ admin/services/provider.php
@@ -1,7 +1,31 @@
-<?php
-// Это точка входа для Service Provider в Joomla 5
-defined('_JEXEC') || exit;
+defined('_JEXEC') or die;
 
-use Joomla\Component\KunenaTopic2Article\Extension\KunenaTopic2ArticleServiceProvider;
+use Joomla\CMS\Extension\ComponentInterface;
+use Joomla\CMS\Extension\Service\Provider\ComponentDispatcherFactory;
+use Joomla\CMS\Extension\Service\Provider\MVCFactory;
+use Joomla\CMS\Dispatcher\ComponentDispatcherFactoryInterface;
+use Joomla\CMS\MVC\Factory\MVCFactoryInterface;
+use Joomla\DI\Container;
+use Joomla\DI\ServiceProviderInterface;
 
-return new KunenaTopic2ArticleServiceProvider;
+// Явная загрузка класса перед использованием
+require_once JPATH_ADMINISTRATOR.'/components/com_kunenatopic2article/src/Extension/KunenaTopic2ArticleServiceProvider.php';
+
+return new class implements ServiceProviderInterface {
+    public function register(Container $container)
+    {
+        $container->registerServiceProvider(new ComponentDispatcherFactory('\\Joomla\\Component\\KunenaTopic2Article'));
+        $container->registerServiceProvider(new MVCFactory('\\Joomla\\Component\\KunenaTopic2Article'));
+        
+        $container->set(
+            ComponentInterface::class,
+            function (Container $container) {
+                $component = new \Joomla\Component\KunenaTopic2Article\Extension\KunenaTopic2ArticleComponent(
+                    $container->get(ComponentDispatcherFactoryInterface::class)
+                );
+                $component->setMVCFactory($container->get(MVCFactoryInterface::class));
+                return $component;
+            }
+        );
+    }
+};
