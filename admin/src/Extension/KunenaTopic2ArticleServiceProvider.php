7cd3e21e20ae770bf3348891cff076ac52eb238c
lemira <lemirat@gmail.com>
2025-06-13 18:06:46 +0200

    Update KunenaTopic2ArticleServiceProvider.php
    namespace для фабрик должны быть без \\Administrator в конце

--- admin/src/Extension/KunenaTopic2ArticleServiceProvider.php
+++ admin/src/Extension/KunenaTopic2ArticleServiceProvider.php
@@ -1,4 +1,6 @@
 <?php
+defined('_JEXEC') || exit;
+
 namespace Joomla\Component\KunenaTopic2Article\Administrator\Extension;
 
 use Joomla\CMS\Extension\ComponentInterface;
@@ -10,18 +12,13 @@ use Joomla\DI\Container;
 use Joomla\DI\ServiceProviderInterface;
 use Joomla\Component\KunenaTopic2Article\Administrator\Extension\KunenaTopic2ArticleComponent;
 
-
 class KunenaTopic2ArticleServiceProvider implements ServiceProviderInterface
 {
     public function register(Container $container): void
     {
-        // Подключаем фабрики MVC и Dispatcher
-  //      $container->registerServiceProvider(new ComponentDispatcherFactory('\\Joomla\\Component\\KunenaTopic2Article'));
-  //      $container->registerServiceProvider(new MVCFactory('\\Joomla\\Component\\KunenaTopic2Article'));
-        $container->registerServiceProvider(new ComponentDispatcherFactory('\\Joomla\\Component\\KunenaTopic2Article\\Administrator'));
-        $container->registerServiceProvider(new MVCFactory('\\Joomla\\Component\\KunenaTopic2Article\\Administrator'));
+        $container->registerServiceProvider(new ComponentDispatcherFactory('\\Joomla\\Component\\KunenaTopic2Article'));
+        $container->registerServiceProvider(new MVCFactory('\\Joomla\\Component\\KunenaTopic2Article'));
 
-        // Регистрируем сам компонент
         $container->set(
             ComponentInterface::class,
             function (Container $container) {
