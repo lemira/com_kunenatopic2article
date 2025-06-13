dc15f61ee8738056c06a089b9403683e7f3fc367
lemira <lemirat@gmail.com>
2025-06-13 18:56:21 +0200

    Update KunenaTopic2ArticleServiceProvider.php
    минимальный рабочий вариант ??
    13.6 кл

--- admin/src/Extension/KunenaTopic2ArticleServiceProvider.php
+++ admin/src/Extension/KunenaTopic2ArticleServiceProvider.php
@@ -4,10 +4,10 @@ namespace Joomla\Component\KunenaTopic2Article\Administrator\Extension;
 use Joomla\CMS\Extension\ComponentInterface;
 use Joomla\CMS\Extension\Service\Provider\ComponentDispatcherFactory;
 use Joomla\CMS\Extension\Service\Provider\MVCFactory;
+use Joomla\CMS\HTML\Registry;
 use Joomla\CMS\MVC\Factory\MVCFactoryInterface;
 use Joomla\DI\Container;
 use Joomla\DI\ServiceProviderInterface;
-use Joomla\Component\KunenaTopic2Article\Administrator\Extension\KunenaTopic2ArticleComponent;
 
 class KunenaTopic2ArticleServiceProvider implements ServiceProviderInterface
 {
@@ -19,10 +19,12 @@ class KunenaTopic2ArticleServiceProvider implements ServiceProviderInterface
         $container->set(
             ComponentInterface::class,
             function (Container $container) {
-                $component = new KunenaTopic2ArticleComponent(
-                    $container->get('ComponentDispatcherFactory')
+                $component = new \Joomla\Component\KunenaTopic2Article\Administrator\Extension\KunenaTopic2ArticleComponent(
+                    $container->get(\Joomla\CMS\Dispatcher\ComponentDispatcherFactoryInterface::class)
                 );
+                
                 $component->setMVCFactory($container->get(MVCFactoryInterface::class));
+                
                 return $component;
             }
         );
