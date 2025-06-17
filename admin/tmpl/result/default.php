3523fcafe002964f28cad2df83203c3b390b039d
lemira <lemirat@gmail.com>
2025-06-17 19:13:34 +0200

    Update default.php
    Полная замена кода. Он только отображает данные
    17.6 дж

--- admin/tmpl/result/default.php
+++ admin/tmpl/result/default.php
@@ -1,17 +1,41 @@
 <?php
+/**
+ * @package     Joomla.Administrator
+ * @subpackage  com_kunenatopic2article
+ *
+ * @copyright   (C) 2025 lr. All rights reserved.
+ * @license     GNU/GPL
+ */
+
 defined('_JEXEC') or die;
 
-use Joomla\CMS\MVC\View\HtmlView as BaseHtmlView;
-use Joomla\CMS\Factory;
+/**
+ * Указываем IDE, что $this - это объект нашего View класса.
+ * @var Joomla\Component\KunenaTopic2Article\Administrator\View\Result\HtmlView $this
+ */
+?>
 
-class KunenaTopic2ArticleViewResult extends BaseHtmlView
-{
-    protected $links;
-
-    public function display($tpl = null)
-    {
-        $app = Factory::getApplication();
-        $this->links = $app->getUserState('com_kunenatopic2article.article_links', []);
-        parent::display($tpl);
-    }
-}
+<div class="joomla-overview">
+    <h1>Результаты работы компонента</h1>
+    
+    <?php if (!empty($this->links)) : ?>
+        <div class="alert alert-success">
+            <p>Были успешно созданы или обновлены следующие материалы:</p>
+            <ul>
+                <?php foreach ($this->links as $link) : ?>
+                    <li>
+                        <a href="<?php echo $link['url']; ?>" target="_blank">
+                            <?php echo htmlspecialchars($link['title'], ENT_QUOTES, 'UTF-8'); ?>
+                        </a>
+                    </li>
+                <?php endforeach; ?>
+            </ul>
+        </div>
+    <?php else : ?>
+        <div class="alert alert-info">
+            <p>Не было создано ни одной статьи или информация о них не найдена.</p>
+        </div>
+    <?php endif; ?>
+    
+   <p>Администратор, вы можете сообщить автору темы о созданных на ее основе статьях</p>
+</div>
