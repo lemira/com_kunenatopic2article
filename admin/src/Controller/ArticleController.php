4787147632008929f5fb41ebd0fe93f8054df2b9
lemira <lemirat@gmail.com>
2025-07-16 19:47:21 +0200

    Update ArticleController.php
    изм для J5 дс

--- admin/src/Controller/ArticleController.php
+++ admin/src/Controller/ArticleController.php
@@ -17,7 +17,8 @@ use Joomla\CMS\Factory;
 use Joomla\CMS\Language\Text;
 use Joomla\CMS\Session\Session; // ? еще нужно?
 use Joomla\CMS\Uri\Uri;
-use Joomla\CMS\User\UserHelper;
+use Joomla\CMS\User\User;
+use Joomla\Component\Users\Administrator\Model\UsersModel;
 
 /**
  * Article Controller
@@ -109,51 +110,69 @@ class ArticleController extends BaseController
      * @param   array  $articleLinks  Массив ссылок на статьи
      * @return  boolean  True в случае успеха, False в случае ошибки
      */
+use Joomla\CMS\Factory;
+use Joomla\CMS\User\User;
+use Joomla\Component\Users\Administrator\Model\UsersModel;
+
 protected function sendLinksToAdministrator(array $articleLinks): void
 {
-    $app = Factory::getApplication();
-    $config = Factory::getConfig();
-    $mailer = Factory::getMailer();
-
-    // 1. Получаем email суперадминистратора (первого из группы Super Users)
-    $superAdminEmail = Factory::getUser(UserHelper::getUsersByGroup(8)[0]->id)->email;
+    // 1. Получаем модель пользователей (J5)
+    $model = Factory::getApplication()->bootComponent('com_users')
+                ->getMVCFactory()
+                ->createModel('Users', 'Administrator', ['ignore_request' => true]);
     
-    // 2. Данные автора (по условиям компонента автор всегда зарегистрирован)
+    // 2. Настраиваем фильтры (группа Super Users = 8)
+    $model->setState('filter.group_id', 8);
+    $model->setState('list.start', 0);
+    $model->setState('list.limit', 1); // Только первый пользователь
+    
+    // 3. Получаем суперадминистратора
+    $superUsers = $model->getItems();
+    
+    if (empty($superUsers)) {
+        throw new RuntimeException('В системе не найден суперадминистратор');
+    }
+    
+    $superAdmin = $superUsers[0];
+    $superAdminEmail = $superAdmin->email;
+
+    // 4. Получаем данные автора (гарантированно зарегистрированного)
     $author = Factory::getUser($this->topicAuthorId);
     $authorEmail = $author->email;
 
-    // 3. Формируем содержимое письма
+    // 5. Формируем и отправляем письма
+    $config = Factory::getConfig();
+    $mailer = Factory::getMailer();
     $siteName = $config->get('sitename');
-    $postId = (int) $this->params->topic_selection;
-    $topicLink = Uri::root() . 'index.php?option=com_kunena&view=topic&postid=' . $postId;
-
-    $articleList = implode("\n", array_map(
-        fn($link) => "- {$link['title']}: {$link['url']}",
-        $articleLinks
-    ));
-
+    
     $subject = Text::sprintf('COM_KUNENATOPIC2ARTICLE_MAIL_SUBJECT', $siteName);
     $body = Text::sprintf(
         'COM_KUNENATOPIC2ARTICLE_MAIL_BODY',
         $siteName,
         $this->subject,
-        $topicLink,
+        Uri::root() . 'index.php?option=com_kunena&view=topic&postid=' . (int)$this->params->topic_selection,
         $author->name,
-        $articleList
+        implode("\n", array_map(
+            fn($link) => "- {$link['title']}: {$link['url']}",
+            $articleLinks
+        ))
     );
 
-    // 4. Отправка писем
-    foreach ([$superAdminEmail, $authorEmail] as $email) {
-        $mailer->clearAllRecipients()
-               ->setSender([$config->get('mailfrom'), $siteName])
-               ->addRecipient($email)
-               ->setSubject($subject)
-               ->setBody($body)
-               ->Send();
+    // 6. Отправка (с обработкой ошибок)
+    try {
+        foreach ([$superAdminEmail, $authorEmail] as $email) {
+            $mailer->clearAllRecipients()
+                   ->setSender([$config->get('mailfrom'), $siteName])
+                   ->addRecipient($email)
+                   ->setSubject($subject)
+                   ->setBody($body)
+                   ->Send();
+        }
+        
+        $this->emailsSent = true;
+        $this->emailsSentTo = [$superAdminEmail, $authorEmail];
+    } catch (MailException $e) {
+        throw new RuntimeException('Ошибка отправки письма: ' . $e->getMessage());
     }
-
-    // 5. Сохраняем статус отправки
-    $this->emailsSent = true;
-    $this->emailsSentTo = [$superAdminEmail, $authorEmail];
 }
 }
