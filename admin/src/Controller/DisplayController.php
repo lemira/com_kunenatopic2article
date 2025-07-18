dddcc7b011965711f2aaaa0a13411e189c6f7569
lemira <lemirat@gmail.com>
2025-07-18 22:49:41 +0200

    Update DisplayController.php
     // Деактивировал кнопку create
      $app->setUserState('com_kunenatopic2article.can_create', false);

--- admin/src/Controller/DisplayController.php
+++ admin/src/Controller/DisplayController.php
@@ -17,7 +17,7 @@ class DisplayController extends BaseController
     public function display($cachable = false, $urlparams = array())
 {
      // Деактивируем кнопку create
-   //    $app->setUserState('com_kunenatopic2article.can_create', false);
+  $app->setUserState('com_kunenatopic2article.can_create', false);
     
     // Всегда используем view по умолчанию ('topic')
     $this->input->set('view', $this->default_view);
