95689b5ebdeff57b2f31fc87c2e66e313494dde7
lemira <lemirat@gmail.com>
2025-07-22 17:32:01 +0200

    Update provider.php
    доб прямой доступ к обоим представлениям коп.
    ОПАСНО: KunenaTopic2ArticleServiceProvider не вызывается ко

--- admin/services/provider.php
+++ admin/services/provider.php
@@ -1,4 +1,18 @@
 <?php
 defined('_JEXEC') || exit;
+
+use Joomla\Component\KunenaTopic2Article\Administrator\View\Result\HtmlView as ResultHtmlView;
+use Joomla\Component\KunenaTopic2Article\Administrator\View\Topic\HtmlView as TopicHtmlView;
+
+return [
+    'views' => [
+        'result.html' => ResultHtmlView::class,
+        'topic.html' => TopicHtmlView::class,
+    ],
+];
+
+/** <?php
+defined('_JEXEC') || exit;
 use Joomla\Component\KunenaTopic2Article\Administrator\Extension\KunenaTopic2ArticleServiceProvider;
 return new KunenaTopic2ArticleServiceProvider;
+**/
