3b6583778aeeb511edbb69062e22bbb00bd3329b
lemira <lemirat@gmail.com>
2025-07-24 17:25:10 +0200

    Update DisplayController.php
    Убран __construct().
    Модель загружается в начале каждого метода.
    Состояние can_create устанавливается внутри методов.

--- admin/src/Controller/DisplayController.php
+++ admin/src/Controller/DisplayController.php
@@ -14,52 +14,45 @@ class DisplayController extends BaseController
     /** @var \Joomla\CMS\MVC\Model\BaseDatabaseModel */
     protected $model;
 
-    public function __construct($config = [])
-    {
-        parent::__construct($config);
-        // Инициализация модели один раз для всех методов
-        $this->model = $this->getModel('Topic');
-        if (!$this->model) {
-            throw new \RuntimeException('Model Topic not loaded');
-        }
-        // Деактивируем кнопку Create при первоначальном вызове контроллера
-        Factory::getApplication()->setUserState('com_kunenatopic2article.can_create', false);
-    }
-
     public function save()
     {
         $this->checkToken();
         $data = $this->input->get('jform', [], 'array');
 
-        // Выполняем сохранение
+        // Загружаем модель динамически
+        $this->model = $this->getModel('Topic');
+        if (!$this->model) {
+            throw new \RuntimeException('Model Topic not loaded in save');
+        }
+
         if ($this->model->save($data)) {
             $message = Text::_('COM_KUNENATOPIC2ARTICLE_PARAMS_SAVED');
             $type = 'success';
-            // Активируем кнопку Create после успешного сохранения
             Factory::getApplication()->setUserState('com_kunenatopic2article.can_create', true);
         } else {
             $message = Text::_('COM_KUNENATOPIC2ARTICLE_SAVE_FAILED');
             $type = 'error';
         }
 
-        // Создаем представление topic напрямую
         $view = $this->getView('topic', 'html');
         if (!$view) {
             throw new \RuntimeException('View object not created for topic');
         }
 
-        // Передаем сообщение и тип в представление
         $view->message = $message;
         $view->messageType = $type;
-
-        // Отображаем представление
         $view->display();
         return true;
     }
 
     public function reset()
     {
-        // Выполняем сброс
+        // Загружаем модель динамически
+        $this->model = $this->getModel('Topic');
+        if (!$this->model) {
+            throw new \RuntimeException('Model Topic not loaded in reset');
+        }
+
         if ($this->model->reset()) {
             $message = Text::_('COM_KUNENATOPIC2ARTICLE_PARAMS_RESET');
             $type = 'success';
@@ -68,20 +61,15 @@ class DisplayController extends BaseController
             $type = 'error';
         }
 
-        // Деактивируем кнопку Create после сброса параметров
         Factory::getApplication()->setUserState('com_kunenatopic2article.can_create', false);
 
-        // Создаем представление topic напрямую
         $view = $this->getView('topic', 'html');
         if (!$view) {
             throw new \RuntimeException('View object not created for topic');
         }
 
-        // Передаем сообщение и тип в представление
         $view->message = $message;
         $view->messageType = $type;
-
-        // Отображаем представление
         $view->display();
         return true;
     }
