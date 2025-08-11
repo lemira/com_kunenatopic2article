cdc6f160dd4d123556cc3f7622f8125e51cf09bc
lemira <lemirat@gmail.com>
2025-08-11 17:23:55 +0200

    Update default.php
    доб стили прямо в <iframe> дс

--- admin/tmpl/topic/default.php
+++ admin/tmpl/topic/default.php
@@ -97,20 +97,20 @@ document.addEventListener('DOMContentLoaded', function() {
                 // Создаем модальное окно для Joomla 4+
                 const modalId = 'previewModal';
                 const modalHtml = `
-                    <div class="modal fade" id="${modalId}" tabindex="-1" aria-hidden="true">
-                        <div class="modal-dialog modal-xl">
-                            <div class="modal-content">
-                                <div class="modal-header">
-                                    <h5 class="modal-title"><?= Text::_('COM_KUNENATOPIC2ARTICLE_PREVIEW_TITLE', true); ?></h5>
-                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                                </div>
-                                <div class="modal-body p-0">
-                                    <iframe src="${result.data.url}" style="width:100%; height:600px; border:none;"></iframe>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                `;
+    <div class="modal fade" id="${modalId}" tabindex="-1" aria-hidden="true">
+        <div class="modal-dialog modal-xl">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title"><?= Text::_('COM_KUNENATOPIC2ARTICLE_PREVIEW_TITLE', true); ?></h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body p-0">
+                    <iframe src="${result.data.url}" style="width:100%; height:600px; border:none; background:white;"></iframe>
+                </div>
+            </div>
+        </div>
+    </div>
+`;
                 
                 // Добавляем модальное окно в DOM
                 document.body.insertAdjacentHTML('beforeend', modalHtml);
