efc78f24ae0038662953eb9a52b68e92d62e644b
lemira <lemirat@gmail.com>
2025-09-02 22:57:40 +0200

    Update default.php
    убрал вычисл-е времени и сообщ-е про 24 часа

--- admin/tmpl/result/default.php
+++ admin/tmpl/result/default.php
@@ -4,11 +4,8 @@ defined('_JEXEC') or die;
 use Joomla\CMS\Factory;
 use Joomla\CMS\Language\Text;
 use Joomla\CMS\Router\Route;
-use Joomla\CMS\Date\Date;
 
 /** @var \Joomla\Component\KunenaTopic2Article\Administrator\View\Result\HtmlView $this */
-
-$currentTime = (new Date())->format('Y-m-d H:i');  // Вычисляем текущее время 
 
 $app = Factory::getApplication();
 ?>
@@ -30,12 +27,7 @@ $app = Factory::getApplication();
             </ul>
         </div>
 
-      <div class="alert alert-info mt-3">
-          <p><?= Text::sprintf('COM_KUNENATOPIC2ARTICLE_ARTICLES_CREATION_TIME', $currentTime) ?></p>
-          <p><?= Text::_('COM_KUNENATOPIC2ARTICLE_ARTICLES_PUBLICATION_TIME') ?></p>
-      </div>
-      
-    <?php else : ?>
+     <?php else : ?>
         <div class="alert alert-info">
             <?php echo Text::_('COM_KUNENATOPIC2ARTICLE_NO_ARTICLES_CREATED'); ?>
         </div>
