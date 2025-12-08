64d53683129ecba6c9663fafa4e735df0c5b2ff7
lemira <lemirat@gmail.com>
2025-12-08 21:50:20 +0100

    Update HtmlView.php
    Added GPL license and defined('_JEXEC') or die;

--- admin/src/View/Topic/HtmlView.php
+++ admin/src/View/Topic/HtmlView.php
@@ -1,9 +1,15 @@
 <?php
-// Файл: admin/src/View/Topic/HtmlView.php
+/**
+ * @package     Joomla.Administrator
+ * @subpackage  com_kunenatopic2article
+ *
+ * @copyright   (C) 2025 Leonid Ratner. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('_JEXEC') or die;
+
 namespace Joomla\Component\KunenaTopic2Article\Administrator\View\Topic;
-
-
-\defined('_JEXEC') or die;
 
 use Joomla\CMS\MVC\View\HtmlView as BaseHtmlView;
 use Joomla\CMS\Factory;
