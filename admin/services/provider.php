b70ea870ef0248edb985f0b23b7939475d0dafcf
lemira <lemirat@gmail.com>
2025-12-08 20:39:21 +0100

    Add package and copyright information to provider.php
    Added package information and copyright notice.

--- admin/services/provider.php
+++ admin/services/provider.php
@@ -1,5 +1,12 @@
 <?php
+/**
+ * @package     Joomla.Administrator
+ * @subpackage  com_kunenatopic2article
+ *
+ * @copyright   (C) 2025 Leonid Ratner. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
 defined('_JEXEC') || exit;
 use Joomla\Component\KunenaTopic2Article\Administrator\Extension\KunenaTopic2ArticleServiceProvider;
 return new KunenaTopic2ArticleServiceProvider;
-
