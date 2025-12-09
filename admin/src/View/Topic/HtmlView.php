f29973498c1ecebd15146c99de0293ae9cdb5b52
lemira <lemirat@gmail.com>
2025-12-09 14:44:42 +0100

    Update HtmlView.php
    namespace ДО defined('_JEXEC')

--- admin/src/View/Topic/HtmlView.php
+++ admin/src/View/Topic/HtmlView.php
@@ -7,9 +7,9 @@
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 
-defined('_JEXEC') or die;
-
 namespace Joomla\Component\KunenaTopic2Article\Administrator\View\Topic;
+
+defined('_JEXEC') or die;
 
 use Joomla\CMS\MVC\View\HtmlView as BaseHtmlView;
 use Joomla\CMS\Factory;
