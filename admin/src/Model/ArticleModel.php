1fb88be30460fcd87a3938379b19a3171eae06eb
lemira <lemirat@gmail.com>
2025-12-10 15:32:05 +0100

    Update ArticleModel.php
    =

--- admin/src/Model/ArticleModel.php
+++ admin/src/Model/ArticleModel.php
@@ -28,6 +28,8 @@ use Joomla\CMS\Filter\InputFilter;
 use Joomla\Component\Content\Site\Helper\RouteHelper;
 use Joomla\CMS\Access\Access;
 use Joomla\CMS\Filter\OutputFilter as FilterOutput;
+use Joomla\Component\Kunenatopic2Article\Administrator\Parser\BBCode; 
+       
 
 /**
  * Article Model
@@ -1173,7 +1175,6 @@ public function sendLinksToAdministrator(array $articleLinks): array
 private function convertBBCodeToHtml($text)
 {
     try {
-        use Joomla\Component\Kunenatopic2Article\Administrator\Parser\BBCode;  // Локальный алиас (внутри функции!)
         $bbcode = new BBCode();
     
     // Уд-м "[br /", которые обрубают текст поста при переносе в статью кл
